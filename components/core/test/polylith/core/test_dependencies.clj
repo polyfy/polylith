d713c5da868db48c13fe3079c4faa3a60fbe967d
tengstrand <joakimtengstand@gmail.com>
2020-05-29 02:03:44 +0200

    Fixed tests.

--- components/core/test/polylith/core/test_dependencies.clj
+++ components/core/test/polylith/core/test_dependencies.clj
@@ -1,26 +1,26 @@
 (ns polylith.core.test-dependencies
   (:require [clojure.test :refer :all]
-            [polylith.common.dependencies :as deps]))
+            [polylith.core.dependencies :as core]))
 
 (deftest dependency--without-top-namespace--returns-dependencies
   (is (= {:ns-path "path"
           :depends-on-interface "file"
           :depends-on-ns "interface"}
-         (deps/dependency "" "common" "path" #{"spec" "cmd" "file" "common"}
+         (core/dependency "" "common" "path" #{"spec" "cmd" "file" "common"}
                           "file.interface"))))
 
 (deftest dependency--with-top-namespace--returns-dependencies
   (is (= {:ns-path "path"
           :depends-on-interface "file"
           :depends-on-ns "interface"}
-         (deps/dependency "polylith." "common" "path" #{"spec" "cmd" "file" "common"}
+         (core/dependency "polylith." "common" "path" #{"spec" "cmd" "file" "common"}
                           "polylith.file.interface"))))
 
 (deftest brick-ns-dependencies--with-top-namespace--returns-dependencies
   (is (= [{:ns-path "/common/readimportsfromdisk.clj"
            :depends-on-interface "file"
            :depends-on-ns "interface"}]
-         (deps/brick-ns-dependencies "polylith." "common" #{"spec" "cmd" "file" "common"}
+         (core/brick-ns-dependencies "polylith." "common" #{"spec" "cmd" "file" "common"}
                                      '{:ns-path "/common/readimportsfromdisk.clj"
                                        :imports [clojure.string
                                                  polylith.file.interface]}))))
@@ -32,7 +32,7 @@
           {:depends-on-interface "user"
            :depends-on-ns        "interface"
            :ns-path              "/common/abc.clj"}]
-         (deps/brick-dependencies "polylith." "common" #{"spec" "cmd" "file" "common" "user"}
+         (core/brick-dependencies "polylith." "common" #{"spec" "cmd" "file" "common" "user"}
                                   '[{:ns-path "/common/readimportsfromdisk.clj"
                                      :imports [clojure.string polylith.file.interface]}
                                     {:ns-path "/common/abc.clj"
@@ -43,7 +43,7 @@
           :illegal-deps [{:ns-path "/common/abc.clj"
                           :depends-on-interface "cmd"
                           :depends-on-ns "core"}]}
-         (deps/dependencies "polylith." "common" #{"spec" "cmd" "file" "common" "user"}
+         (core/dependencies "polylith." "common" #{"spec" "cmd" "file" "common" "user"}
                             '[{:ns-path "/common/readimportsfromdisk.clj"
                                :imports [clojure.string polylith.file.interface]}
                               {:ns-path "/common/abc.clj"
