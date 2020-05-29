5e5e53803df707e0461d07e284a74bd15f5e5ccf
tengstrand <joakimtengstand@gmail.com>
2020-05-29 03:29:21 +0200

    Return errors in 'dependencies'.

--- components/core/src/polylith/core/dependencies.clj
+++ components/core/src/polylith/core/dependencies.clj
@@ -29,14 +29,14 @@
 (defn brick-dependencies [top-ns brick interface-names brick-imports]
   (vec (mapcat #(brick-ns-dependencies top-ns brick interface-names %) brick-imports)))
 
+(defn error [{:keys [ns-path depends-on-interface depends-on-ns]} dir]
+  (when ns-path
+    (str "Illegal dependency on namespace '" depends-on-interface "." depends-on-ns "' in '" dir ns-path
+         "'. Change to '" depends-on-interface ".interface' to solve the problem.")))
+
 (defn dependencies [top-ns bricks-dir brick interface-names brick-imports]
   (let [deps (brick-dependencies top-ns brick interface-names brick-imports)
         interface-deps (vec (sort (set (map :depends-on-interface deps))))
-        {:keys [ns-path
-                depends-on-interface
-                depends-on-ns]} (first (filterv #(not= "interface" (:depends-on-ns %)) deps))
-        error (when ns-path
-                (str "Illegal dependency on namespace '" depends-on-interface "." depends-on-ns "' in '" bricks-dir ns-path
-                     "'. Change to '" depends-on-interface ".interface' to solve the problem."))]
+        errors (filterv identity (map #(error % bricks-dir) (filterv #(not= "interface" (:depends-on-ns %)) deps)))]
     {:dependencies interface-deps
-     :error error}))
+     :errors errors}))
--- components/core/test/polylith/core/test_dependencies.clj
+++ components/core/test/polylith/core/test_dependencies.clj
@@ -17,41 +17,62 @@
                           "polylith.file.interface"))))
 
 (deftest brick-ns-dependencies--with-top-namespace--returns-dependencies
-  (is (= [{:ns-path "/common/readimportsfromdisk.clj"
-           :depends-on-interface "file"
-           :depends-on-ns "interface"}]
-         (core/brick-ns-dependencies "polylith." "common" #{"spec" "cmd" "file" "common"}
-                                     '{:ns-path "/common/readimportsfromdisk.clj"
-                                       :imports [clojure.string
-                                                 polylith.file.interface]}))))
+  (is (= [{:depends-on-interface "cmd"
+           :depends-on-ns        "core"
+           :ns-path              "/common/core.clj"}
+          {:depends-on-interface "invoice"
+           :depends-on-ns        "core"
+           :ns-path              "/common/core.clj"}]
+         (core/brick-ns-dependencies "polylith." "common" #{"spec" "cmd" "file" "invoice"}
+                                     '{:ns-path "/common/core.clj"
+                                       :imports [clojure.core
+                                                 polylith.user.interface
+                                                 polylith.cmd.core
+                                                 polylith.invoice.core]}))))
 
 (deftest brick-dependencies--with-top-namespace--returns-dependencies
-  (is (= [{:depends-on-interface "file"
+  (is (= [{:depends-on-interface "user"
            :depends-on-ns        "interface"
-           :ns-path              "/common/readimportsfromdisk.clj"}
-          {:depends-on-interface "user"
-           :depends-on-ns        "interface"
-           :ns-path              "/common/abc.clj"}]
-         (core/brick-dependencies "polylith." "common" #{"spec" "cmd" "file" "common" "user"}
-                                  '[{:ns-path "/common/readimportsfromdisk.clj"
-                                     :imports [clojure.string polylith.file.interface]}
+           :ns-path              "/common/abc.clj"}
+          {:depends-on-interface "cmd"
+           :depends-on-ns        "core"
+           :ns-path              "/common/abc.clj"}
+          {:depends-on-interface "invoice"
+           :depends-on-ns        "core"
+                    :ns-path              "/common/abc.clj"}]
+         (core/brick-dependencies "polylith." "common" #{"spec" "cmd" "user" "invoice"}
+                                  '[{:ns-path "/common/core.clj"
+                                     :imports [clojure.string
+                                               polylith.file.interface]}
                                     {:ns-path "/common/abc.clj"
-                                     :imports [polylith.user.interface]}]))))
+                                     :imports [clojure.core
+                                               polylith.user.interface
+                                               polylith.cmd.core
+                                               polylith.invoice.core]}]))))
 
 (deftest dependencies--without-errors--returns-dependencies-and-errors
-  (is (= {:dependencies ["cmd" "file" "user"]
-          :error nil}
-         (core/dependencies "polylith." "components" "common" #{"spec" "cmd" "file" "common" "user"}
+  (is (= {:dependencies ["cmd" "file" "invoice" "user"]
+          :errors []}
+         (core/dependencies "polylith." "components" "common" #{"spec" "cmd" "file" "invoice" "user"}
                             '[{:ns-path "/common:/readimportsfromdisk.clj"
-                               :imports [clojure.string polylith.file.interface]}
+                               :imports [clojure.string
+                                         polylith.file.interface]}
                               {:ns-path "/common/abc.clj"
-                               :imports [polylith.user.interface polylith.cmd.interface]}]))))
+                               :imports [clojure.core
+                                         polylith.user.interface
+                                         polylith.cmd.interface.v2.core
+                                         polylith.invoice.interface]}]))))
 
 (deftest dependencies--with-errors--returns-dependencies-and-errors
-  (is (= {:dependencies ["cmd" "file" "user"]
-          :error "Illegal dependency on namespace 'cmd.core' in 'components/common/abc.clj'. Change to 'cmd.interface' to solve the problem."}
-         (core/dependencies "polylith." "components" "common" #{"spec" "cmd" "file" "common" "user"}
-                            '[{:ns-path "/common:/readimportsfromdisk.clj"
-                               :imports [clojure.string polylith.file.interface]}
-                              {:ns-path "/common/abc.clj"
-                               :imports [polylith.user.interface polylith.cmd.core]}]))))
+  (is (= {:dependencies ["cmd" "file" "invoice" "user"]
+          :errors       ["Illegal dependency on namespace 'invoice.core' in 'components/common/purchase.clj'. Change to 'invoice.interface' to solve the problem."
+                         "Illegal dependency on namespace 'cmd.core' in 'components/common/billing.clj'. Change to 'cmd.interface' to solve the problem."]}
+         (core/dependencies "polylith." "components" "common" #{"spec" "cmd" "file" "invoice" "user"}
+                            '[{:ns-path "/common/purchase.clj"
+                               :imports [clojure.string
+                                         polylith.file.interface
+                                         polylith.invoice.core]}
+                              {:ns-path "/common/billing.clj"
+                               :imports [clojure.core
+                                         polylith.user.interface
+                                         polylith.cmd.core]}]))))
