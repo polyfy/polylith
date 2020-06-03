9ad085fed530ac17ffd13b50759f8d62f9f5ac7e
tengstrand <joakimtengstand@gmail.com>
2020-06-03 18:03:06 +0200

    Fixed failing tests.

--- components/workspace/test/polylith/workspace/validate/interface_deps_test.clj
+++ components/workspace/test/polylith/workspace/validate/interface_deps_test.clj
@@ -6,33 +6,32 @@
   (let [component '{:name "common"
                     :type "component"
                     :interface {:name "common"}
-                    :imports [{:ns-path "/common:/readimportsfromdisk.clj"
+                    :imports [{:ns-path "common:/readimportsfromdisk.clj"
                                :imports [clojure.string
                                          polylith.file.interface]}
-                              {:ns-path "/common/abc.clj"
+                              {:ns-path "common/abc.clj"
                                :imports [clojure.core
                                          polylith.user.interface
                                          polylith.cmd.interface.v2.core
                                          polylith.invoice.interface]}]}]
     (is (= []
-           (ideps/errors "polylith." component #{"spec" "cmd" "file" "invoice" "user"} [])))))
-
+           (ideps/brick-errors "polylith." component #{"spec" "cmd" "file" "invoice" "user"} [])))))
 
 (deftest dependencies--with-errors--returns-errors
   (let [component {:name "common"
                    :type "component"
                    :interface {:name "common"}
-                   :imports '[{:ns-path "/common/purchase.clj"
+                   :imports '[{:ns-path "common/purchase.clj"
                                :imports [clojure.string
                                          polylith.file.interface
                                          polylith.invoice.core]}
-                              {:ns-path "/common/billing.clj"
+                              {:ns-path "common/billing.clj"
                                :imports [clojure.core
                                          polylith.user.interface
                                          polylith.cmd.core]}]}]
-    (is (= ["Illegal dependency on namespace 'invoice.core' in 'components//common/purchase.clj'. Import 'invoice.interface' instead to solve the problem."
-            "Illegal dependency on namespace 'cmd.core' in 'components//common/billing.clj'. Import 'cmd.interface' instead to solve the problem."]
-           (ideps/errors "polylith." component #{"spec" "cmd" "file" "invoice" "user"} [])))))
+    (is (= ["Illegal dependency on namespace 'invoice.core' in 'components/common/purchase.clj'. Use 'invoice.interface' instead to solve the problem."
+            "Illegal dependency on namespace 'cmd.core' in 'components/common/billing.clj'. Use 'cmd.interface' instead to solve the problem."]
+           (ideps/brick-errors "polylith." component #{"spec" "cmd" "file" "invoice" "user"} [])))))
 
 (deftest dependencies--component-with-mismatching-interface-name--returns-no-errors
   (let [component '{:type "component",
