68b1eb98ad32f44415fa39721b27b4996c790212
tengstrand <joakimtengstand@gmail.com>
2020-06-07 20:36:40 +0200

    Cleaned up the test.

--- components/workspace/test/polylith/workspace/validate/illegal_name_sharing_test.clj
+++ components/workspace/test/polylith/workspace/validate/illegal_name_sharing_test.clj
@@ -2,19 +2,15 @@
   (:require [clojure.test :refer :all]
             [polylith.workspace.validate.illegal-name-sharing :as illegal-name-sharing]))
 
-(def interfaces '[{:name "mybase",
-                   :declarations [{:name add-two, :type function, :signature [x]}],
-                   :implemented-by ["mybase"],
-                   :dependencies []}])
+(def interfaces '[{:name "mybase"
+                   :declarations [{:name add-two, :type function, :signature [x]}]
+                   :implemented-by ["mybase"]}])
 
-(def components '[{:name "mybase",
-                   :type "component",
-                   :imports [{:ns-path "mybase/interface.clj", :imports [mybase1.core]}
-                             {:ns-path "mybase/core.clj", :imports []}],
-                   :interface {:name "mybase", :declarations [{:name add-two, :type function, :signature [x]}]},
-                   :dependencies []}])
+(def components '[{:name "mybase1"
+                   :interface {:name "mybase1",
+                               :declarations [{:name add-two, :type function, :signature [x]}]}}])
 
-(def bases [{:name "mybase", :type "base", :imports [{:ns-path "mybase/core.clj", :imports []}], :dependencies []}])
+(def bases [{:name "mybase",}])
 
 (def interface-names (map :name interfaces))
 
