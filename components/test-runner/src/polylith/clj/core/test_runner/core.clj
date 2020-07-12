73a03c02a640a51921aac33aab8511983852e2a7
tengstrand <joakimtengstand@gmail.com>
2020-07-12 11:30:49 +0200

    Only run tests for changed bricks.

--- components/test-runner/src/polylith/clj/core/test_runner/core.clj
+++ components/test-runner/src/polylith/clj/core/test_runner/core.clj
@@ -1,7 +1,8 @@
 (ns polylith.clj.core.test-runner.core
-  (:require [clojure.tools.deps.alpha :as tools-deps]
+  (:require [clojure.set :as set]
+            [clojure.string :as str]
+            [clojure.tools.deps.alpha :as tools-deps]
             [polylith.clj.core.common.interfc :as common]
-            [polylith.clj.core.file.interfc :as file]
             [polylith.clj.core.util.interfc :as util]
             [polylith.clj.core.util.interfc.color :as color])
   (:refer-clojure :exclude [test]))
@@ -29,45 +30,54 @@
       (println e)
       (throw e))))
 
-(defn ->test-namespaces [bases components test-base-names test-component-names]
-  (let [base-name->namespaces (into {} (map (juxt :name :namespaces-test) bases))
-        component-name->namespaces (into {} (map (juxt :name :namespaces-test) components))
-        base-namespaces (mapv :namespace (mapcat base-name->namespaces test-base-names))
-        component-namespaces (mapv :namespace (mapcat component-name->namespaces test-component-names))]
-    (concat base-namespaces component-namespaces)))
+(defn ->test-namespaces [bricks test-brick-names]
+  (let [brick-name->namespaces (into {} (map (juxt :name :namespaces-test) bricks))]
+    (mapv :namespace (mapcat brick-name->namespaces test-brick-names))))
 
 (defn run-tests-for-environment [{:keys [bases components] :as workspace}
-                                 {:keys [test-base-names test-component-names paths test-paths] :as environment}]
+                                 {:keys [name test-base-names test-component-names paths test-paths] :as environment}
+                                 {:keys [changed-components changed-bases indirect-changes]}]
   (when (-> test-paths empty? not)
     (let [color-mode (-> workspace :settings :color-mode)
           config (->config workspace environment)
           lib-paths (resolve-deps config)
           src-paths (set (concat paths test-paths))
           paths (concat src-paths lib-paths)
-          test-namespaces (->test-namespaces bases components test-base-names test-component-names)
+          bricks (concat components bases)
+          changed-bricks (set (concat changed-components changed-bases (indirect-changes name)))
+          brick-names (set (concat test-base-names test-component-names))
+          bricks-to-test (vec (sort (set/intersection brick-names changed-bricks)))
+          component-names (set (map :name components))
+          base-names (set (map :name bases))
+          bases-to-test-msg (color/base (str/join ", " (filter #(contains? base-names %) bricks-to-test)) color-mode)
+          components-to-test-msg (color/component (str/join ", " (filter #(contains? component-names %) bricks-to-test)) color-mode)
+          bricks-to-test-msg (str/join ", " [components-to-test-msg bases-to-test-msg])
+          test-namespaces (->test-namespaces bricks bricks-to-test)
           test-statements (map ->test-statement test-namespaces)
           class-loader (common/create-class-loader paths color-mode)]
-      (doseq [statement test-statements]
-        (let [{:keys [error fail pass] :as summary}
-              (try
-                (common/eval-in class-loader statement)
-                (catch Exception e
-                  (.printStackTrace e)
-                  (println (str (color/error color-mode "Couldn't run test statement: ") statement " " (color/error color-mode e)))))
-              result-str (str "Test results: " pass " passes, " fail " failures, " error " errors.")]
-          (when (or (nil? error)
-                    (< 0 error)
-                    (< 0 fail))
-            (throw (Exception. (str "\n" (color/error color-mode result-str)) summary)))
-          (println (str "\n" (color/ok color-mode result-str))))))))
+      (when (-> test-statements empty? not)
+        (println (str "\nRuning tests for the " (color/environment name color-mode) " environment in " (count test-namespaces) " namespaces: " bricks-to-test-msg))
+        (doseq [statement test-statements]
+          (let [{:keys [error fail pass] :as summary}
+                (try
+                  (common/eval-in class-loader statement)
+                  (catch Exception e
+                    (.printStackTrace e)
+                    (println (str (color/error color-mode "Couldn't run test statement: ") statement " " (color/error color-mode e)))))
+                result-str (str "Test results: " pass " passes, " fail " failures, " error " errors.")]
+            (when (or (nil? error)
+                      (< 0 error)
+                      (< 0 fail))
+              (throw (Exception. (str "\n" (color/error color-mode result-str)) summary)))
+            (println (str "\n" (color/ok color-mode result-str)))))))))
 
-(defn run-all-tests [workspace environments]
+(defn run-all-tests [workspace environments changes]
   (doseq [environment environments]
-    (run-tests-for-environment workspace environment)))
+    (run-tests-for-environment workspace environment changes)))
 
-(defn run [{:keys [environments] :as workspace} env]
+(defn run [{:keys [environments changes] :as workspace} env]
   (if (nil? env)
-    (run-all-tests workspace environments)
+    (run-all-tests workspace environments changes)
     (if-let [environment (util/find-first #(= env (:name %)) environments)]
-      (run-tests-for-environment workspace environment)
+      (run-tests-for-environment workspace environment changes)
       (println (str "Can't find environment '" env "'.")))))
