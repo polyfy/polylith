0e8d9f0a5fb4bee8f5f79bf24009ba9a8e430f38
tengstrand <joakimtengstand@gmail.com>
2020-07-21 12:35:14 +0200

    Added :vcs to :settings in :workspace.

--- components/workspace-clj/src/polylith/clj/core/workspace_clj/core.clj
+++ components/workspace-clj/src/polylith/clj/core/workspace_clj/core.clj
@@ -22,14 +22,15 @@
    (let [config (read-string (slurp (str ws-path "/deps.edn")))]
      (workspace-from-disk ws-path config)))
   ([ws-path {:keys [polylith]}]
-   (let [{:keys [top-namespace color-mode env-short-names]} polylith
+   (let [{:keys [vcs top-namespace color-mode env-short-names]} polylith
          top-ns (common/top-namespace top-namespace)
          top-src-dir (str/replace top-ns "." "/")
          component-names (file/directory-paths (str ws-path "/components"))
          components (components-from-disk/read-components ws-path top-src-dir component-names)
          bases (bases-from-disk/read-bases ws-path top-src-dir)
          environments (envs-from-disk/read-environments ws-path)
-         settings (util/ordered-map :top-namespace top-namespace
+         settings (util/ordered-map :vcs (or vcs "git")
+                                    :top-namespace top-namespace
                                     :color-mode color-mode
                                     :env-short-names env-short-names)]
      (util/ordered-map :ws-path ws-path
