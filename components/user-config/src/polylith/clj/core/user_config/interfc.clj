c60ff1b1222f92c4a1d501dfdc76e010da319664
tengstrand <joakimtengstand@gmail.com>
2020-07-22 04:10:35 +0200

    Don't throw an exception if ~/.polylith/config.edn doesn't exists.

--- components/user-config/src/polylith/clj/core/user_config/interfc.clj
+++ components/user-config/src/polylith/clj/core/user_config/interfc.clj
@@ -1,7 +1,10 @@
 (ns polylith.clj.core.user-config.interfc)
 
 (defn config-content []
-  (read-string (slurp (str (System/getProperty "user.home") "/.polylith/config.edn"))))
+  (try
+    (read-string (slurp (str (System/getProperty "user.home") "/.polylith/config.edn")))
+    (catch Exception _
+      {})))
 
 (defn thousand-separator []
   (:thousand-separator (config-content) ","))
