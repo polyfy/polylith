970dfb501639efe5200a2f1da397b462396d2e42
tengstrand <joakimtengstand@gmail.com>
2020-07-23 12:17:43 +0200

    Force giving a top namespace in the 'create w' command + treat '-' as an empty namespace.

--- components/create/test/polylith/clj/core/create/workspace_test.clj
+++ components/create/test/polylith/clj/core/create/workspace_test.clj
@@ -4,6 +4,13 @@
 
 (use-fixtures :each helper/test-setup-and-tear-down)
 
+(deftest create-workspace--missing-namespace--prints-out-error-message
+  (let [ws-name "ws1"
+        output (with-out-str
+                 (helper/execute-command "" "create" "w" ws-name))]
+    (is (= "A namespace must be given, or - if omitted.\n"
+           output))))
+
 (deftest create-workspace--creates-empty-directories-and-a-deps-edn-config-file
   (let [ws-name "ws1"
         output (with-out-str
@@ -22,6 +29,28 @@
 
     (is (= [{:polylith {:vcs "git"
                         :top-namespace "se.example"
+                        :interface-ns "interface"
+                        :env-short-names {}}}]
+           (helper/content ws-name "deps.edn")))))
+
+(deftest create-workspace--creates-workspace-with-an-empty-top-namespace
+  (let [ws-name "ws1"
+        output (with-out-str
+                 (helper/execute-command "" "create" "w" ws-name "-"))]
+    (is (= ""
+           output))
+
+    (is (= #{"bases"
+             "components"
+             "environments"
+             "images"
+             "images/logo.png"
+             "deps.edn"
+             "readme.md"}
+           (helper/paths ws-name)))
+
+    (is (= [{:polylith {:vcs "git"
+                        :top-namespace ""
                         :interface-ns "interface"
                         :env-short-names {}}}]
            (helper/content ws-name "deps.edn")))))
