e02a0794d26b0b111d84a36f6d48e9b1848e5913
tengstrand <joakimtengstand@gmail.com>
2020-08-05 23:13:57 +0200

    Added tests for validation 202, missing libraries.

--- components/validate/src/polylith/clj/core/validate/m202_missing_library.clj
+++ components/validate/src/polylith/clj/core/validate/m202_missing_library.clj
@@ -9,17 +9,17 @@
   (or (= namespace lib-ns)
       (str/starts-with? namespace (common/suffix-ns-with-dot lib-ns))))
 
-(defn brick-imports [brick-name name->brick]
-  (mapcat :imports (-> brick-name name->brick :namespaces-src)))
+(defn brick-imports [brick-name name->ns-src]
+  (mapcat :imports (name->ns-src brick-name)))
 
 (defn expected-ns [used-ns ns-libs]
   (util/find-first #(included-in-ns? % used-ns) ns-libs))
 
-(defn env-status [{:keys [name component-names base-names lib-deps]} top-ns name->brick ns->lib]
+(defn env-status [{:keys [name component-names base-names lib-deps]} top-ns name->ns-src ns->lib]
   (let [brick-names (concat component-names base-names)
         ns-libs (reverse (sort (map #(-> % first str) ns->lib)))
         used-namespaces (set (filter #(not (included-in-ns? top-ns %))
-                                     (mapcat #(brick-imports % name->brick) brick-names)))
+                                     (mapcat #(brick-imports % name->ns-src) brick-names)))
         expected-libs (set (map ns->lib (set (map #(expected-ns % ns-libs) used-namespaces))))
         used-libs (set (map key lib-deps))
         missing-libs (set/difference expected-libs used-libs #{nil})]
@@ -41,6 +41,6 @@
     (missing-lib-warning env missing-libraries color-mode)))
 
 (defn warnings [environments components bases ns->lib top-ns color-mode]
-  (let [name->brick (into {} (map (juxt :name identity) (concat bases components)))]
+  (let [name->ns-src (into {} (map (juxt :name :namespaces-src) (concat bases components)))]
     (mapcat #(env-warning % color-mode)
-            (map #(env-status % top-ns name->brick ns->lib) environments))))
+            (map #(env-status % top-ns name->ns-src ns->lib) environments))))
