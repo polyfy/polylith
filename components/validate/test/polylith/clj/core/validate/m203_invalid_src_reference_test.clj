ab3db4397d7023f5b8c52bc2e0c54cbae96fe7d4
tengstrand <joakimtengstand@gmail.com>
2020-08-06 08:02:26 +0200

    Added test for validation 203, invalid src reference.

---
+++ components/validate/test/polylith/clj/core/validate/m203_invalid_src_reference_test.clj
@@ -0,0 +1,15 @@
+(ns polylith.clj.core.validate.m203-invalid-src-reference-test
+  (:require [clojure.test :refer :all]
+            [polylith.clj.core.validate.m203-invalid-src-reference :as m203]
+            [polylith.clj.core.util.interfc.color :as color]))
+
+(def environments [{:name "cli",
+                    :src-paths ["wrong/path/src"]}])
+
+(deftest warnings--when-one-path-is-non-existing--return-a-warning
+  (is (= [{:type "warning"
+           :code 203
+           :message "Non-existing directories was found for the cli environment and will be ignored: \"wrong/path/src\""
+           :colorized-message "Non-existing directories was found for the cli environment and will be ignored: \"wrong/path/src\""
+           :environment "cli"}])
+      (m203/warnings "." environments color/none)))
