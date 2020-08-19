f29bdebc3785aeee3ce8bbe6271f5e09ffe7ddbb
tengstrand <joakimtengstand@gmail.com>
2020-08-19 14:21:27 +0200

    Show warnings and errors at the end of the 'info' command again.

--- components/workspace/src/polylith/clj/core/workspace/text_table/info_tables.clj
+++ components/workspace/src/polylith/clj/core/workspace/text_table/info_tables.clj
@@ -1,11 +1,15 @@
 (ns polylith.clj.core.workspace.text-table.info-tables
-  (:require [polylith.clj.core.workspace.text-table.count-table :as count-table]
+  (:require [polylith.clj.core.common.interfc :as common]
+            [polylith.clj.core.workspace.text-table.count-table :as count-table]
             [polylith.clj.core.workspace.text-table.env-table :as env-table]
             [polylith.clj.core.workspace.text-table.ws-table :as ws-table]))
 
-(defn print-info [workspace show-loc? show-resources?]
+(defn print-info [{:keys [messages] :as workspace} show-loc? show-resources?]
   (count-table/print-table workspace)
   (println)
   (env-table/print-table workspace show-loc? show-resources?)
   (println)
-  (ws-table/print-table workspace show-loc? show-resources?))
+  (ws-table/print-table workspace show-loc? show-resources?)
+  (when (-> messages empty? not)
+    (println)
+    (println (common/pretty-messages messages (-> workspace :settings :color-mode)))))
