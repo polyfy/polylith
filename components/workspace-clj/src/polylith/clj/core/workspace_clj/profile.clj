2519affbe560a0e66f644e70d5c19bff970714ac
tengstrand <joakimtengstand@gmail.com>
2020-08-20 15:04:27 +0200

    Changed so that profiles now starts with +.

--- components/workspace-clj/src/polylith/clj/core/workspace_clj/profile.clj
+++ components/workspace-clj/src/polylith/clj/core/workspace_clj/profile.clj
@@ -3,12 +3,12 @@
             [polylith.clj.core.util.interfc :as util]))
 
 (defn profile [[profile-key {:keys [extra-paths extra-deps]}]]
-  [(subs (name profile-key) 4)
+  [(subs (name profile-key) 1)
    (util/ordered-map :paths extra-paths
                      :lib-deps (util/stringify-and-sort-map extra-deps))])
 
 (defn profile? [[alias]]
-  (str/starts-with? (name alias) "dev+"))
+  (str/starts-with? (name alias) "+"))
 
 (defn profile->settings [aliases]
   (into {} (map profile
