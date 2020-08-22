f9e7175f627643ab1c996f6f595cc32634af1796
tengstrand <joakimtengstand@gmail.com>
2020-08-22 22:30:28 +0200

    Show active profiles in the 'info' command.

--- components/workspace/src/polylith/clj/core/workspace/text_table/info_tables.clj
+++ components/workspace/src/polylith/clj/core/workspace/text_table/info_tables.clj
@@ -1,11 +1,18 @@
 (ns polylith.clj.core.workspace.text-table.info-tables
-  (:require [polylith.clj.core.common.interfc :as common]
+  (:require [clojure.string :as str]
+            [polylith.clj.core.common.interfc :as common]
+            [polylith.clj.core.util.interfc.color :as color]
             [polylith.clj.core.workspace.text-table.count-table :as count-table]
             [polylith.clj.core.workspace.text-table.env-table :as env-table]
             [polylith.clj.core.workspace.text-table.ws-table :as ws-table]))
 
-(defn print-info [{:keys [messages] :as workspace} show-loc? show-resources?]
+(defn print-active-dev-profiles [{:keys [active-dev-profiles]} {:keys [color-mode]}]
+  (println (str "  Active dev profiles: " (color/profile (str/join ", " (sort active-dev-profiles)) color-mode))))
+
+(defn print-info [{:keys [settings messages user-input] :as workspace} show-loc? show-resources?]
   (count-table/print-table workspace)
+  (println)
+  (print-active-dev-profiles user-input settings)
   (println)
   (env-table/print-table workspace show-loc? show-resources?)
   (println)
