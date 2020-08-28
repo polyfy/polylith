638c3d1380e1ee0cee1f84d92429b0da66f740f7
tengstrand <joakimtengstand@gmail.com>
2020-08-28 09:27:47 +0200

    Fixed an illegal dependency to the deps component.

--- components/command/src/polylith/clj/core/command/deps.clj
+++ components/command/src/polylith/clj/core/command/deps.clj
@@ -1,13 +1,12 @@
 (ns polylith.clj.core.command.deps
   (:require [polylith.clj.core.deps.interfc :as deps]
-            [polylith.clj.core.common.interfc :as common]
-            [polylith.clj.core.deps.text-table.lib-table :as lib-table]))
+            [polylith.clj.core.common.interfc :as common]))
 
 (defn deps [workspace environment-name brick-name unnamed-args show-lib?]
   (let [{:keys [ok? message]} (common/validate-args unnamed-args "deps env:my-env brick:my-brick")]
     (if ok?
       (if show-lib?
-        (lib-table/print-table workspace)
+        (deps/print-lib-table workspace)
         (if environment-name
           (if brick-name
             (deps/print-brick-table workspace environment-name brick-name)
