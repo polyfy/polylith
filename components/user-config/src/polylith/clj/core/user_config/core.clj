2ca0a641d6e88c6e3117d7481af8596a35a6f32f
tengstrand <joakimtengstand@gmail.com>
2020-08-30 10:07:36 +0200

    Print error message if the config file couldn't be read.

--- components/user-config/src/polylith/clj/core/user_config/core.clj
+++ components/user-config/src/polylith/clj/core/user_config/core.clj
@@ -1,16 +1,18 @@
 (ns polylith.clj.core.user-config.core
-  (:require [polylith.clj.core.util.interfc.str :as str-util]))
+  (:require [polylith.clj.core.file.interfc :as file]
+            [polylith.clj.core.util.interfc.str :as str-util]))
 
 (defn home-dir []
   (let [dir (System/getProperty "user.home")]
-    (println "###home-dir=" dir)
-    (str-util/skip-if-ends-with dir "/")))
+    (str-util/skip-if-ends-with dir file/sep)))
 
 (defn- config-content []
-  (try
-    (read-string (slurp (str (home-dir) "/.polylith/config.edn")))
-    (catch Exception _
-      {})))
+  (let [config-dir (str (home-dir) file/sep ".polylith" file/sep "config.edn")]
+    (try
+      (read-string (slurp config-dir))
+      (catch Exception _
+        (println "Couldn't read config file: " config-dir)
+        {}))))
 
 (defn thousand-separator []
   (:thousand-separator (config-content) ","))
