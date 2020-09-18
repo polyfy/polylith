479ca08cf1286cf9926001f140d349a82a88faae
Joakim Tengstrand <joakimtengstrand@gmail.com>
2020-09-18 13:43:51 +0200

    Added build script for the 'api' environment + cleand up the build scripts for 'core'. (#13)

--- components/api/src/polylith/clj/core/api/interface.clj
+++ components/api/src/polylith/clj/core/api/interface.clj
@@ -1,5 +1,6 @@
 (ns polylith.clj.core.api.interface
-  (:require [polylith.clj.core.api.core :as core]))
+  (:require [polylith.clj.core.api.core :as core])
+  (:gen-class))
 
 (defn environments-to-deploy []
   (core/environments-to-deploy))
---
+++ scripts/build-api.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "#### Build api uberjar"
+./build-uberjar.sh api
