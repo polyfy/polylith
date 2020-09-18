e8d71a84ddc864f2d3e1c491d9091699ed5b9118
tengstrand <joakimtengstrand@gmail.com>
2020-09-18 13:39:24 +0200

    Added build script for the 'api' environment + cleand up the build scripts for 'core'.

--- scripts/build-core-api.sh
+++ scripts/build-core-api.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-echo "#### Build core API"
+echo "#### Build Clojure core uberjar"
+./build-uberjar.sh core
+sudo cp ../environments/core/target/core.jar ~/.m2/repository/polylith/polylith-clj-core/1.0/polylith-clj-core-1.0.jar
+
+echo "#### Build Java core API"
 cd ../java
 mvn clean package
 sudo cp target/polylith-java-core-jar-with-dependencies.jar ~/.m2/repository/polylith/polylith-java-core/0.1.0/polylith-java-core-0.1.0.jar
