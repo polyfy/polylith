b0186c27022464bbfb2277137caebe52e23e4bc0
Joakim Tengstrand <joakimtengstrand@gmail.com>
2020-09-25 19:07:21 +0200

    Fixed failing test by removing :size keys from the map that read files from disk. (#20)

--- components/workspace-clj/test/polylith/clj/core/workspace_clj/environment_test.clj
+++ components/workspace-clj/test/polylith/clj/core/workspace_clj/environment_test.clj
@@ -38,33 +38,35 @@
   (is (= "components/comp"
          (env/absolute-path "../../components/comp" "dev"))))
 
+(defn filter-version [[library {:keys [mvn/version]}]]
+  [library {:mvn/version version}])
+
 (deftest environments--config-map-with-aliases--returns-environments
-  (is (= {:config-file     "environments/poly/deps.edn"
-          :dev?            false
-          :env-dir         "environments/poly"
-          :lib-deps        {"org.clojure/clojure"                             {:mvn/version "1.10.1"
-                                                                               :size        3908431}
-                            "org.clojure/tools.deps.alpha"                    {:mvn/version "0.8.695"
-                                                                               :size        47566}
-                            "org.jetbrains.kotlin/kotlin-compiler-embeddable" {:mvn/version "1.3.72"
-                                                                               :size        40201239}}
-          :maven-repos     {"central" {:url "https://repo1.maven.org/maven2/"}
-                            "clojars" {:url "https://repo.clojars.org/"}}
-          :name            "poly"
-          :namespaces-src  []
-          :namespaces-test [{:file-path "environments/poly/test/env/dummy.clj"
-                             :imports   ["clojure.test"]
-                             :name      "env.dummy"
-                             :namespace "env.dummy"}]
-          :src-paths       ["bases/tool/src"
-                            "components/change/src"
-                            "components/common/src"
-                            "components/deps/src"
-                            "components/file/src"
-                            "environments/poly/src"]
-          :test-lib-deps   {}
-          :test-paths      ["bases/tool/test"
-                            "components/change/test"
-                            "components/common/test"]
-          :type            "environment"}
-         (env/read-environment "poly" "environments/poly" "environments/poly/deps.edn" false paths deps aliases mvn/standard-repos))))
+  (let [result (env/read-environment "poly" "environments/poly" "environments/poly/deps.edn" false paths deps aliases mvn/standard-repos)]
+    (is (= {:config-file     "environments/poly/deps.edn"
+            :dev?            false
+            :env-dir         "environments/poly"
+            :maven-repos     {"central" {:url "https://repo1.maven.org/maven2/"}
+                              "clojars" {:url "https://repo.clojars.org/"}}
+            :name            "poly"
+            :namespaces-src  []
+            :namespaces-test [{:file-path "environments/poly/test/env/dummy.clj"
+                               :imports   ["clojure.test"]
+                               :name      "env.dummy"
+                               :namespace "env.dummy"}]
+            :src-paths       ["bases/tool/src"
+                              "components/change/src"
+                              "components/common/src"
+                              "components/deps/src"
+                              "components/file/src"
+                              "environments/poly/src"]
+            :test-lib-deps   {}
+            :test-paths      ["bases/tool/test"
+                              "components/change/test"
+                              "components/common/test"]
+            :type            "environment"}
+           (dissoc result :lib-deps)))
+    (is (= {"org.clojure/clojure"                             {:mvn/version "1.10.1"}
+            "org.clojure/tools.deps.alpha"                    {:mvn/version "0.8.695"}
+            "org.jetbrains.kotlin/kotlin-compiler-embeddable" {:mvn/version "1.3.72"}}
+           (into {} (map filter-version (:lib-deps result)))))))
