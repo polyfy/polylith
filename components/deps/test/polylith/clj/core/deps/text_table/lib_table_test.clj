f07be4d3917b08d05787d93caf33dfa22f231ae4
tengstrand <joakimtengstrand@gmail.com>
2020-09-25 11:10:40 +0200

    Added new KB column to the library table that shows the size of each library in kilobytes.

--- components/deps/test/polylith/clj/core/deps/text_table/lib_table_test.clj
+++ components/deps/test/polylith/clj/core/deps/text_table/lib_table_test.clj
@@ -74,31 +74,34 @@
 
                 :environments [{:alias "core"
                                 :lib-deps {"org.clojure/clojure" {:mvn/version "1.10.2"}
-                                           "org.clojure/tools.deps.alpha" {:mvn/version "0.8.695"}}}
+                                           "org.clojure/tools.deps.alpha" {:mvn/version "0.8.695" :size 55500}}}
                                {:alias "inv"
                                 :lib-deps {"org.clojure/clojure" {:mvn/version "1.10.1"}
-                                           "org.clojure/tools.deps.alpha" {:mvn/version "0.8.695"}}}
+                                           "org.clojure/tools.deps.alpha" {:mvn/version "0.8.695" :size 23400}}}
                                {:alias "dev"
-                                :lib-deps {"org.clojure/clojure" {:mvn/version "1.10.1"}
+                                :lib-deps {"org.clojure/clojure" {:mvn/version "1.10.1" :size 112200}
                                            "org.clojure/tools.deps.alpha" {:mvn/version "0.8.695"}}}]})
 
 (deftest table--components-with-library-dependencies--returns-correct-table
-  (is (= ["                                                                                                                                  w   "
-          "                                                                                                                                  o   "
-          "                                                                                                    p     t  t     u              r   "
-          "                                                                                                    a     e  e  t  s  u           k   "
-          "                                                                                                    t     s  s  e  e  s     v  w  s   "
-          "                                                                                                    h     t  t  x  r  e     a  o  p   "
-          "                                                                               c     c              -     -  -  t  -  r     l  r  a   "
-          "                                                                            c  o  c  r              f     h  r  -  c  -     i  k  c   "
-          "                                                                            h  m  o  e              i  s  e  u  t  o  i     d  s  e   "
-          "                                                                            a  m  m  a  d  f     h  n  h  l  n  a  n  n  u  a  p  -   "
-          "                                                                            n  a  m  t  e  i  g  e  d  e  p  n  b  f  p  t  t  a  c  c"
-          "                                                                            g  n  o  o  p  l  i  l  e  l  e  e  l  i  u  i  o  c  l  l"
-          "  library                       version   core  inv   dev  default  admin   e  d  n  r  s  e  t  p  r  l  r  r  e  g  t  l  r  e  j  i"
-          "  -------------------------------------   ---------   -------------------   ----------------------------------------------------------"
-          "  org.clojure/clojure           1.10.1     -     x     x      -       -     x  x  x  ·  x  x  x  x  x  x  x  x  x  ·  x  x  x  x  x  ·"
-          "  org.clojure/clojure           1.10.2     x     -     -      -       -     x  x  x  ·  x  x  x  x  x  x  x  x  x  ·  x  x  x  x  x  ·"
-          "  org.clojure/tools.deps.alpha  0.8.695    x     x     x      -       -     ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  x  ·  ·  ·  ·  ·  ·  x  ·"
-          "  zprint                        0.4.15     -     -     -      -       x     ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·"]
+  (is (= ["                                                                                                                                       w   "
+          "                                                                                                                                       o   "
+          "                                                                                                         p     t  t     u              r   "
+          "                                                                                                         a     e  e  t  s  u           k   "
+          "                                                                                                         t     s  s  e  e  s     v  w  s   "
+          "                                                                                                         h     t  t  x  r  e     a  o  p   "
+          "                                                                                    c     c              -     -  -  t  -  r     l  r  a   "
+          "                                                                                 c  o  c  r              f     h  r  -  c  -     i  k  c   "
+          "                                                                                 h  m  o  e              i  s  e  u  t  o  i     d  s  e   "
+          "                                                                                 a  m  m  a  d  f     h  n  h  l  n  a  n  n  u  a  p  -   "
+          "                                                                                 n  a  m  t  e  i  g  e  d  e  p  n  b  f  p  t  t  a  c  c"
+          "                                                                                 g  n  o  o  p  l  i  l  e  l  e  e  l  i  u  i  o  c  l  l"
+          "  library                       version   KB   core   inv  dev  default   admin  e  d  n  r  s  e  t  p  r  l  r  r  e  g  t  l  r  e  j  i"
+          "  ------------------------------------------   ----   -----------------   -----------------------------------------------------------------"
+          "  org.clojure/clojure           1.10.1   109    -      -    x      -        -    x  x  x  ·  x  x  x  x  x  x  x  x  x  ·  x  x  x  x  x  ·"
+          "  org.clojure/clojure           1.10.1          -      x    -      -        -    x  x  x  ·  x  x  x  x  x  x  x  x  x  ·  x  x  x  x  x  ·"
+          "  org.clojure/clojure           1.10.2          x      -    -      -        -    x  x  x  ·  x  x  x  x  x  x  x  x  x  ·  x  x  x  x  x  ·"
+          "  org.clojure/tools.deps.alpha  0.8.695         -      -    x      -        -    ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  x  ·  ·  ·  ·  ·  ·  x  ·"
+          "  org.clojure/tools.deps.alpha  0.8.695   54    x      -    -      -        -    ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  x  ·  ·  ·  ·  ·  ·  x  ·"
+          "  org.clojure/tools.deps.alpha  0.8.695   22    -      x    -      -        -    ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  x  ·  ·  ·  ·  ·  ·  x  ·"
+          "  zprint                        0.4.15          -      -    -      -        x    ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·  ·"]
          (lib-table/table workspace))))
