6591857bf37526557f980233b5a9bd19e09d0fbf
Joakim Tengstrand <joakimtengstrand@gmail.com>
2020-10-11 08:47:30 +0200

    Updated poly-git-sha to be419d4e6976d88e2a755e3f62689f944e28ceb4. (#35)

--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -6,4 +6,4 @@
 (def version "0.1.0-alpha2+")
 (def date "2020-10-11")
 
-(def poly-git-sha "34804e54538fad540b69498996c1b6a6adecf67c")
+(def poly-git-sha "be419d4e6976d88e2a755e3f62689f944e28ceb4")
