9dfad8d32ff9cb47bc8d5a684a007b02a7d1000c
tengstrand <joakimtengstrand@gmail.com>
2020-10-17 11:33:19 +0200

    Use LATEST for the git sha.

--- components/creator/src/polylith/clj/core/creator/workspace.clj
+++ components/creator/src/polylith/clj/core/creator/workspace.clj
@@ -51,7 +51,7 @@
    (str "            :poly {:main-opts [\"-m\" \"polylith.clj.core.poly_cli.poly\"]")
    (str "                   :extra-deps {polyfy/polylith")
    (str "                                {:git/url   \"https://github.com/polyfy/polylith\"")
-   (str "                                 :sha       \"" version/poly-git-sha "\"")
+   (str "                                 :sha       \"LATEST\"")
    (str "                                 :deps/root \"projects/poly\"}}}}}")])
 
 (defn create-user-config-if-not-exists []
