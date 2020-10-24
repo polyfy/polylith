19625b45c174b751802c47cc7b8e6f5614b9f07c
tengstrand <joakimtengstrand@gmail.com>
2020-10-24 19:42:57 +0200

    Fixe a bug in "help create X" command. Use "create component" instead of "create c" as default. Moved Commands from the wiki to doc/commands.md. Updated the doc with input from James. Updated the readme.

--- components/help/src/polylith/clj/core/help/create.clj
+++ components/help/src/polylith/clj/core/help/create.clj
@@ -9,23 +9,21 @@
   (str "  Creates a component, base, project or workspace.\n"
        "\n"
        "  poly create " (s/key "TYPE" cm) " [" (s/key "ARGS" cm) "]\n"
-       "    " (s/key "TYPE" cm) " = " (s/key "c" cm) " -> Creates a component.\n"
-       "           " (s/key "b" cm) " -> Creates a base.\n"
-       "           " (s/key "p" cm) " -> Creates a project.\n"
-       "           " (s/key "w" cm) " -> Creates a workspace.\n"
+       "    " (s/key "TYPE" cm) " = " (s/key "c[omponent]" cm) " -> Creates a component.\n"
+       "           " (s/key "b[ase]     " cm) " -> Creates a base.\n"
+       "           " (s/key "p[roject]  " cm) " -> Creates a project.\n"
+       "           " (s/key "w[orkspace]" cm) " -> Creates a workspace.\n"
        "\n"
        "    " (s/key "ARGS" cm) " = Varies depending on " (s/key "TYPE" cm) ". To get help for a specific " (s/key "TYPE" cm) ", type:\n"
        "             poly help create " (s/key "TYPE" cm) "\n"
        "\n"
-       "  Not only " (s/key "c" cm) ", " (s/key "b" cm) ", " (s/key "p" cm) " and " (s/key "w" cm) " can be used for " (s/key "TYPE" cm) " but also " (s/key "component" cm) ", " (s/key "base" cm) "\n"
-       "  " (s/key "project" cm) " and " (s/key "workspace" cm) ".\n"
-       "\n"
        "  Example:\n"
        "    poly create c name:user\n"
-       "    poly create c name:admin interface:user\n"
-       "    poly create b name:mybase\n"
-       "    poly create p name:myproject\n"
-       "    poly create w name:myws top-ns:com.my.company"))
+       "    poly create component name:user\n"
+       "    poly create component name:admin interface:user\n"
+       "    poly create base name:mybase\n"
+       "    poly create project name:myproject\n"
+       "    poly create workspace name:myws top-ns:com.my.company"))
 
 (defn print-help [ent color-mode]
   (case ent
