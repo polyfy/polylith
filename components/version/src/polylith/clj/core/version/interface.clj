2defec64b95db25c2f749b3b95c1fa8a352ede94
Furkan Bayraktar <me@furkanbayraktar.com>
2020-12-01 08:20:09 +0100

    Updated version to 0.1.0-alpha7

--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -3,5 +3,5 @@
 (def ws-schema-version {:breaking 0
                         :non-breaking 0})
 
-(def version "0.1.0-alpha6")
-(def date "2020-11-26")
+(def version "0.1.0-alpha7")
+(def date "2020-12-01")
