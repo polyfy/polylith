polyfy/polylith

commit 969c7bc1c10a2f21e5aa0250ce671a029d59975d

Add calva jack in settings (#77)
* Create VS Code settings for Calva

* Git ignore some Calva files

* Make created .gitignore more complete

* Add Calva settings to this project

components/creator/src/polylith/clj/core/creator/workspace.clj
@@ -5,7 +5,41 @@
 
 (def gitignore-content
   ["**/classes"
-   "**/target"])
+   "**/target"
+   "**/.artifacts"
+   "**/.cpcache"
+   "**/.DS_Store"
+   "**/.gradle"
+   ""
+   "# User-specific stuff"
+   ".idea/**/workspace.xml"
+   ".idea/**/tasks.xml"
+   ".idea/**/usage.statistics.xml"
+   ".idea/**/shelf"
+   ".idea/**/statistic.xml"
+   ".idea/dictionaries/**"
+   ".idea/libraries/**"
+   ""
+   "# File-based project format"
+   "*.iws"
+   "*.ipr"
+   ""
+   "# Cursive Clojure plugin"
+   ".idea/replstate.xml"
+   "*.iml"
+   ""
+   "/example/example/**"
+   "artifacts"
+   "projects/**/pom.xml"
+   ""
+   "# nrepl"
+   ".nrepl-port"
+   ""
+   "# clojure-lsp"
+   ".lsp/sqlite*.db"
+   ""
+   "# Calva VS Code Extension"
+   ".calva/output-window/output.calva-repl"])
 
 (defn readme-content [ws-name]
   ["<img src=\"logo.png\" width=\"30%\" alt=\"Polylith\" id=\"logo\">"
@@ -51,6 +85,20 @@
    (str "                                 :sha       \"" sha "\"")
    (str "                                 :deps/root \"projects/poly\"}}}}}")])
 
+(defn calva-settings-content [ws-name]
+  [(str "{")
+   (str "    \"calva.replConnectSequences\": [")
+   (str "        {")
+   (str "            \"projectType\": \"deps.edn\",")
+   (str "            \"name\": \"" ws-name "\",")
+   (str "            \"cljsType\": \"none\",")
+   (str "            \"menuSelections\": {")
+   (str "                \"cljAliases\": [\"dev\", \"test\"]")
+   (str "            }")
+   (str "        }")
+   (str "    ]")
+   (str "}")])
+
 (defn create-ws [ws-dir ws-name top-ns create-ws-dir? git-repo? insert-sha? sha branch is-git-add]
   (when create-ws-dir?
     (file/create-dir ws-dir))
@@ -63,6 +111,7 @@
   (file/create-file (str ws-dir "/workspace.edn") (workspace-content top-ns is-git-add))
   (file/create-file (str ws-dir "/deps.edn") (deps-content sha))
   (file/create-file (str ws-dir "/readme.md") (readme-content ws-name))
+  (file/create-file (str ws-dir ".vscode/settings.json") (calva-settings-content ws-name))
   (file/create-file (str ws-dir "/development/src/.keep") [""])
   (file/create-file (str ws-dir "/components/.keep") [""])
   (file/create-file (str ws-dir "/bases/.keep") [""])

.gitignore
@@ -29,5 +29,11 @@ artifacts
 projects/**/pom.xml
 /scripts/output/example/**
 /examples/doc-example/.idea/**
-/examples/local-dep/.idea/**
+/examples/local-dep/.idea/**ZZ
 /examples/local-dep-old-format/.idea/**
+
+# clojure-lsp
+.lsp/sqlite*.db
+
+# Calva
+.calva/output-window/output.calva-repl

commit 06550596f5da8912d1e18ab736f82069cfcf26cb

Fix bug introduced by PR #77.

components/creator/src/polylith/clj/core/creator/workspace.clj
@@ -93,7 +93,7 @@
    (str "            \"name\": \"" ws-name "\",")
    (str "            \"cljsType\": \"none\",")
    (str "            \"menuSelections\": {")
-   (str "                \"cljAliases\": [\"dev\", \"test\"]")
+   (str "                \"cljAliases\": [\"dev\", \"test\", \"test\"]")
    (str "            }")
    (str "        }")
    (str "    ]")
@@ -107,11 +107,12 @@
   (file/create-dir (str ws-dir "/development"))
   (file/create-dir (str ws-dir "/development/src"))
   (file/create-dir (str ws-dir "/projects"))
+  (file/create-dir (str ws-dir "/.vscode"))
   (file/create-file (str ws-dir "/.gitignore") gitignore-content)
   (file/create-file (str ws-dir "/workspace.edn") (workspace-content top-ns is-git-add))
   (file/create-file (str ws-dir "/deps.edn") (deps-content sha))
   (file/create-file (str ws-dir "/readme.md") (readme-content ws-name))
-  (file/create-file (str ws-dir ".vscode/settings.json") (calva-settings-content ws-name))
+  (file/create-file (str ws-dir "/.vscode/settings.json") (calva-settings-content ws-name))
   (file/create-file (str ws-dir "/development/src/.keep") [""])
   (file/create-file (str ws-dir "/components/.keep") [""])
   (file/create-file (str ws-dir "/bases/.keep") [""])

components/creator/test/polylith/clj/core/creator/base_test.clj
@@ -25,6 +25,8 @@
 
     (is (= #{".git"
              ".gitignore"
+             ".vscode"
+             ".vscode/settings.json"
              "bases"
              "bases/.keep"
              "bases/my-base"
@@ -48,9 +50,9 @@
              "development"
              "development/src"
              "development/src/.keep"
+             "logo.png"
              "projects"
              "projects/.keep"
-             "logo.png"
              "readme.md"
              "workspace.edn"}
            (helper/paths "ws1")))

components/creator/test/polylith/clj/core/creator/component_test.clj
@@ -25,6 +25,8 @@
 
     (is (= #{".git"
              ".gitignore"
+             ".vscode"
+             ".vscode/settings.json"
              "bases"
              "bases/.keep"
              "components"
@@ -77,6 +79,8 @@
 
     (is (= #{".git"
              ".gitignore"
+             ".vscode"
+             ".vscode/settings.json"
              "bases"
              "bases/.keep"
              "components"

components/creator/test/polylith/clj/core/creator/project_test.clj
@@ -26,6 +26,8 @@
 
     (is (= #{".git"
              ".gitignore"
+             ".vscode"
+             ".vscode/settings.json"
              "bases"
              "bases/.keep"
              "components"

components/creator/test/polylith/clj/core/creator/workspace_test.clj
@@ -21,6 +21,8 @@
 
     (is (= #{".git"
              ".gitignore"
+             ".vscode"
+             ".vscode/settings.json"
              "bases"
              "bases/.keep"
              "components"
@@ -36,6 +38,8 @@
              "workspace.edn"
              "ws2"
              "ws2/.gitignore"
+             "ws2/.vscode"
+             "ws2/.vscode/settings.json"
              "ws2/bases"
              "ws2/bases/.keep"
              "ws2/components"
@@ -72,6 +76,8 @@
 
     (is (= #{".git"
              ".gitignore"
+             ".vscode"
+             ".vscode/settings.json"
              "bases"
              "bases/.keep"
              "components"
@@ -114,6 +120,20 @@
             (str "                                 :sha       \"SHA\"")
             (str "                                 :deps/root \"projects/poly\"}}}}}")]
            (helper/content "ws1" "deps.edn")))
+
+    (is (= ["{"
+            "    \"calva.replConnectSequences\": ["
+            "        {"
+            "            \"projectType\": \"deps.edn\","
+            "            \"name\": \"ws1\","
+            "            \"cljsType\": \"none\","
+            "            \"menuSelections\": {"
+            "                \"cljAliases\": [\"dev\", \"test\", \"test\"]"
+            "            }"
+            "        }"
+            "    ]"
+            "}"]
+           (helper/content "ws1" ".vscode/settings.json")))
 
     (is (= ["{:top-namespace \"se.example\""
             " :interface-ns \"interface\""