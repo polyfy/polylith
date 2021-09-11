ecb3d1cdf268a0b9f0bfd8f9c7b15971bf895209
Joakim Tengstrand <joakimtengstrand@gmail.com>
2021-09-11 10:55:47 +0200

    Allow the 'data-readers' namespace to exist in other places that the top namespaces.

---
+++ examples/local-dep-old-format/components/database/src/readers/data_readers.clj
@@ -0,0 +1,5 @@
+(ns data-readers)
+
+;; Clojure source files have to live under the top namespace.
+;; The exception is the data-reader namespace ('data_readers.clj' and 'data_readers.cljc')
+;; that is allowed to live in any namespace.
