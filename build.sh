887c30d11453430f90f661ed641190e69232a144
Sean Corfield <sean@corfield.org>
2021-10-06 22:33:37 -0700

    fix path in copy #134

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 echo "#### Build poly uberjar"
 clojure -T:build uberjar :project poly
-sudo cp ../projects/poly/target/poly.jar /usr/local/polylith/poly.jar
+sudo cp projects/poly/target/poly.jar /usr/local/polylith/poly.jar
