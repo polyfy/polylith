35cc2b5e5721306bda6ccaaaf8e32b67b59af33a
Joakim Tengstrand <joakimtengstrand@gmail.com>
2022-03-18 18:27:38 +0100

    Return with 1 if check fails when running the test command (#192)
    Exit with 1 if we have validation errors when executing the 'test' command.

--- components/command/src/polylith/clj/core/command/test.clj
+++ components/command/src/polylith/clj/core/command/test.clj
@@ -2,8 +2,11 @@
   (:require [polylith.clj.core.test-runner.interface :as test-runner]
             [polylith.clj.core.common.interface :as common]))
 
-(defn run [workspace unnamed-args is-verbose color-mode]
+(defn run
+  "Return true if the tests could be executed correctly."
+  [workspace unnamed-args test-result is-verbose color-mode]
   (let [{:keys [ok? message]} (common/validate-args unnamed-args "test project:dev")]
     (if ok?
-      (test-runner/run workspace is-verbose color-mode)
+      (reset! test-result
+              (test-runner/run workspace is-verbose color-mode))
       (println message))))
