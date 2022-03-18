2a18e1ff1246c0da90dce10e67ebb4cf68865d20
Joakim Tengstrand <joakimtengstrand@gmail.com>
2022-03-18 18:43:13 +0100

    Remove the text that says that people can contact the Polylith Team via the Forum, in the readme.md created by the 'create workspace' command.

--- components/creator/test/polylith/clj/core/creator/workspace_test.clj
+++ components/creator/test/polylith/clj/core/creator/workspace_test.clj
@@ -101,7 +101,7 @@
             "- The [Polylith Tool documentation](https://github.com/polyfy/polylith)"
             "- The [RealWorld example app documentation](https://github.com/furkan3ayraktar/clojure-polylith-realworld-example-app)"
             ""
-            "You can also get in touch with the Polylith Team via our [forum](https://polylith.freeflarum.com) or on [Slack](https://clojurians.slack.com/archives/C013B7MQHJQ)."
+            "You can also get in touch with the Polylith Team on [Slack](https://clojurians.slack.com/archives/C013B7MQHJQ)."
             ""
             "<h1>ws1</h1>"
             ""
--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -4,7 +4,7 @@
 (def major 0)
 (def minor 2)
 (def patch 14)
-(def revision "alpha-issue177-01")
+(def revision "alpha-issue157-01")
 (def name (str major "." minor "." patch "-" revision))
 
 (def date "2022-03-18")
