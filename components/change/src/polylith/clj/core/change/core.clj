822ea055b6198ea831640d378df2b2d6e543e173
Joakim Tengstrand <joakimtengstrand@gmail.com>
2022-03-21 07:13:49 +0100

    Implement project-to-projects-to-test (work in progress).

--- components/change/src/polylith/clj/core/change/core.clj
+++ components/change/src/polylith/clj/core/change/core.clj
@@ -32,7 +32,7 @@
         affected-projects (affected-projects projects changed-components changed-bases changed-projects)
         project-to-indirect-changes (indirect/project-to-indirect-changes projects-deps changed-bricks)
         project-to-bricks-to-test (bricks-to-test/project-to-bricks-to-test changed-projects projects settings changed-components changed-bases project-to-indirect-changes selected-bricks selected-projects is-dev is-run-all-brick-tests)
-        project-to-projects-to-test (projects-to-test/project-to-projects-to-test projects affected-projects paths is-dev is-run-project-tests is-all)]
+        project-to-projects-to-test (projects-to-test/project-to-projects-to-test projects paths affected-projects selected-projects is-dev is-run-project-tests is-all)]
     (util/ordered-map :since since
                       :since-sha since-sha
                       :since-tag tag
--- components/change/src/polylith/clj/core/change/projects_to_test.clj
+++ components/change/src/polylith/clj/core/change/projects_to_test.clj
@@ -4,7 +4,7 @@
             [polylith.clj.core.path-finder.interface.select :as select]
             [polylith.clj.core.path-finder.interface.extract :as extract]))
 
-(defn included-projects [{:keys [paths]} disk-paths]
+(defn existing-projects [{:keys [paths]} disk-paths]
   (let [path-entries (extract/from-paths paths disk-paths)]
     (select/names path-entries c/project? c/test-path? c/exists?)))
 
@@ -20,13 +20,24 @@
                                    (set included-projects))]
     (select-projects project-name projects is-dev)))
 
-(defn projects-to-test [{:keys [name is-run-tests] :as project} disk-paths affected-projects is-dev is-run-project-tests is-all]
-  (let [included-projects (included-projects project disk-paths)]
+;; todo: fix
+;(defn is-run-tests []
+;
+;  (or (and (not is-dev)
+;           (or is-run-all-brick-tests
+;               (empty? selected-projects)))
+;      (or (contains? selected-projects project-name)
+;          (contains? selected-projects alias))))
+
+
+(defn projects-to-test [{:keys [is-dev name] :as project} disk-paths affected-projects selected-projects is-dev-user-input is-run-project-tests is-all]
+  (let [is-run-tests true ;; todo: fix
+        existing-projects (existing-projects project disk-paths)]
     (cond
-      is-all [name (vec (sort (select-projects name included-projects is-dev)))]
-      (and is-run-tests is-run-project-tests) [name (vec (sort (project-tests name affected-projects included-projects is-dev)))]
+      is-all [name (vec (sort (select-projects name existing-projects is-dev-user-input)))]
+      (and is-run-tests is-run-project-tests) [name (vec (sort (project-tests name affected-projects existing-projects is-dev)))]
       :else [name []])))
 
-(defn project-to-projects-to-test [projects affected-projects disk-paths is-dev is-run-project-tests is-all]
-  (into {} (map #(projects-to-test % disk-paths affected-projects is-dev is-run-project-tests is-all)
+(defn project-to-projects-to-test [projects disk-paths affected-projects selected-projects is-dev-user-input is-run-project-tests is-all]
+  (into {} (map #(projects-to-test % disk-paths affected-projects selected-projects is-dev-user-input is-run-project-tests is-all)
                 projects)))
