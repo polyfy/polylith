71b1be31dd3e1f523731fdac7af36a2edb19fb7a
Imre Kószó <imrekoszo@users.noreply.github.com>
2022-03-22 13:04:00 +0100

    Fix some dysfunctional test assertions

--- components/change/test/polylith/clj/core/change/bricks_to_test_test.clj
+++ components/change/test/polylith/clj/core/change/bricks_to_test_test.clj
@@ -52,27 +52,27 @@
                   "rest-api"
                   "tag"
                   "user"]
-          "development" []})
-    (test {:changed-projects []
-           :settings {}
-           :changed-components ["article"]
-           :changed-bases []
-           :project-to-indirect-changes {}
-           :selected-bricks nil
-           :is-run-all-brick-tests true})))
+          "development" []}
+         (test {:changed-projects []
+                :settings {}
+                :changed-components ["article"]
+                :changed-bases []
+                :project-to-indirect-changes {}
+                :selected-bricks nil
+                :is-run-all-brick-tests true}))))
 
 (deftest project-to-bricks-to-test--with-run-all-selected-only-test-two-bricks--returns-the-two-bricks-for-active-projects
   (is (= {"cli" []
           "core" ["tag"
                   "user"]
-          "development" []})
-    (test {:changed-projects []
-           :settings {:projects {"core" {:test {:include ["tag" "user"]}}}}
-           :changed-components ["article"]
-           :changed-bases []
-           :project-to-indirect-changes {}
-           :selected-bricks nil
-           :is-run-all-brick-tests false})))
+          "development" []}
+         (test {:changed-projects []
+                :settings {:projects {"core" {:test {:include ["tag" "user"]}}}}
+                :changed-components ["article"]
+                :changed-bases []
+                :project-to-indirect-changes {}
+                :selected-bricks nil
+                :is-run-all-brick-tests true}))))
 
 (deftest project-to-bricks-to-test--when-the-project-itself-has-changed--return-all-bricks-for-that-project
   (is (= {"cli" []
@@ -82,14 +82,14 @@
                   "rest-api"
                   "tag"
                   "user"]
-          "development" []})
-    (test {:changed-projects ["core"]
-           :settings {}
-           :changed-components ["article"]
-           :changed-bases []
-           :project-to-indirect-changes {}
-           :selected-bricks nil
-           :is-run-all-brick-tests false})))
+          "development" []}
+         (test {:changed-projects ["core"]
+                :settings {}
+                :changed-components ["article"]
+                :changed-bases []
+                :project-to-indirect-changes {}
+                :selected-bricks nil
+                :is-run-all-brick-tests false}))))
 
 (deftest project-to-bricks-to-test--with-two-changed-components-and-one-selected-brick--returns-selected-bricks-that-are-also-changed
   (is (= {"cli" []
