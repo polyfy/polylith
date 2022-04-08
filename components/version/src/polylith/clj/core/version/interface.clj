28e0e7c65c2d0e908669e46aa12cb6969c7cb77a
Imre Kószó <imrekoszo@users.noreply.github.com>
2022-04-08 11:36:32 +0200

    Bump revision

--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -4,7 +4,7 @@
 (def major 0)
 (def minor 2)
 (def patch 15)
-(def revision "alpha-pr196-01")
+(def revision "alpha-pr196-02")
 (def name (str major "." minor "." patch "-" revision))
 
 (def date "2022-04-08")
