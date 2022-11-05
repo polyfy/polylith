205d3392dd5a468f40ce4f482ed4b9689cf872b7
Joakim Tengstrand <joakimtengstrand@gmail.com>
2022-11-05 09:01:50 +0100

    Update ws version.

--- components/validator/test/polylith/clj/core/validator/m206_unreadable_namespace_test.clj
+++ components/validator/test/polylith/clj/core/validator/m206_unreadable_namespace_test.clj
@@ -22,10 +22,10 @@
 (deftest warning--when-having-unreadable-namespaces--returns-warnings
   (is (= [{:type "warning",
            :code 206,
-           :message "Unreadable namespaces in poly-cli: /Users/joakimtengstrand/source/polylith/bases/poly-cli/src/polylith/clj/core/poly_cli/core.clj",
-           :colorized-message "Unreadable namespaces in poly-cli: /Users/joakimtengstrand/source/polylith/bases/poly-cli/src/polylith/clj/core/poly_cli/core.clj"}
+           :message "Unreadable namespace in poly-cli: /Users/joakimtengstrand/source/polylith/bases/poly-cli/src/polylith/clj/core/poly_cli/core.clj",
+           :colorized-message "Unreadable namespace in poly-cli: /Users/joakimtengstrand/source/polylith/bases/poly-cli/src/polylith/clj/core/poly_cli/core.clj"}
           {:type "warning",
            :code 206,
-           :message "Unreadable namespaces in poly-cli: /Users/joakimtengstrand/source/polylith/bases/poly-cli/test/polylith/clj/core/poly_cli/api_argument_mapping_test.clj",
-           :colorized-message "Unreadable namespaces in poly-cli: /Users/joakimtengstrand/source/polylith/bases/poly-cli/test/polylith/clj/core/poly_cli/api_argument_mapping_test.clj"}]
+           :message "Unreadable namespace in poly-cli: /Users/joakimtengstrand/source/polylith/bases/poly-cli/test/polylith/clj/core/poly_cli/api_argument_mapping_test.clj",
+           :colorized-message "Unreadable namespace in poly-cli: /Users/joakimtengstrand/source/polylith/bases/poly-cli/test/polylith/clj/core/poly_cli/api_argument_mapping_test.clj"}]
          (m206/warnings bricks nil nil "none"))))
--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -1,13 +1,14 @@
+x
 (ns polylith.clj.core.version.interface
   (:refer-clojure :exclude [name]))
 
 (def major 0)
 (def minor 2)
 (def patch 16)
-(def revision "alpha-issue254")
+(def revision "alpha-issue254-01")
 (def name (str major "." minor "." patch "-" revision))
 
-(def date "2022-11-04")
+(def date "2022-11-05")
 
 (defn version
   ([ws-type]
@@ -24,12 +25,15 @@
                         :date date}
               :ws {:type :toolsdeps2
                    :breaking 1
-                   :non-breaking 1}}
+                   :non-breaking 2}}
              from (assoc :from from)))))
 
 ;; === workspace attributes (ws) ===
 ;;
 ;; ws     release         action    attribute
-;; -----  -------------   -------   ------------------------------------
+;; -----  -------------   -------   -----------------------------------------------------------------
+;; 1.2    0.2.16-alpha    added     :components > COMPONENT > :namespaces > :src/test > [] > :invalid
+;;                        added     :bases/projects > BASE > :namespaces > :src/test > [] > :invalid
+;;                        added     :projects > PROJECT > :namespaces > :src/test > [] > :invalid
 ;; 1.1    0.2.14-alpha    added     :settings > :vcs > :is-git-repo
 ;;                        deleted   :projects > PROJECT > :is-run-tests
