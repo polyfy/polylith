fe125b1a52e4028936f5bc35519d8d15f73a42eb
Furkan Bayraktar <me@furkanbayraktar.com>
2022-11-21 00:49:23 +0100

    Added more tests to check enrich-settings

--- components/workspace/test/polylith/clj/core/workspace/settings_test.clj
+++ components/workspace/test/polylith/clj/core/workspace/settings_test.clj
@@ -37,3 +37,54 @@
                      "helpers"        {:alias "h"
                                        :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}}}
          (settings/enrich-settings {:projects {"helpers" {:alias "h"}}} projects))))
+
+(deftest enrich-settings--a-set-of-projects-with-incomplete-project-mapping-and-with-empty-test-runner-config--returns-dev-and-undefined-mappings
+  (is (= {:projects {"backend-system" {:alias "?4"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "banking-system" {:alias "?2"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "car"            {:alias "?3"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "clojure"        {:alias "?1"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "development"    {:alias "dev"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "helpers"        {:alias "h"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}}}
+         (settings/enrich-settings {:projects {"helpers" {:alias "h"
+                                                          :test {:create-test-runner []}}}}
+                                   projects))))
+
+(deftest enrich-settings--a-set-of-projects-with-incomplete-project-mapping-and-with-empty-test-config--returns-dev-and-undefined-mappings
+  (is (= {:projects {"backend-system" {:alias "?4"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "banking-system" {:alias "?2"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "car"            {:alias "?3"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "clojure"        {:alias "?1"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "development"    {:alias "dev"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "helpers"        {:alias "h"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}}}
+         (settings/enrich-settings {:projects {"helpers" {:alias "h"
+                                                          :test {}}}}
+                                   projects))))
+
+(deftest enrich-settings--a-set-of-projects-with-incomplete-project-mapping-and-with-test-runner--returns-dev-and-undefined-mappings
+  (is (= {:projects {"backend-system" {:alias "?4"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "banking-system" {:alias "?2"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "car"            {:alias "?3"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "clojure"        {:alias "?1"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "development"    {:alias "dev"
+                                       :test  {:create-test-runner ['polylith.clj.core.clojure-test-test-runner.interface/create]}}
+                     "helpers"        {:alias "h"
+                                       :test  {:create-test-runner [:default]}}}}
+         (settings/enrich-settings {:projects {"helpers" {:alias "h"
+                                                          :test {:create-test-runner [:default]}}}}
+                                   projects))))
