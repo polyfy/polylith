a1581ccc87009127f2151579b826258dd57e3077
Furkan Bayraktar <me@furkanbayraktar.com>
2022-12-09 13:15:57 +0100

    Bumped the version to v0.2.17 (#267)

--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -3,11 +3,11 @@
 
 (def major 0)
 (def minor 2)
-(def patch 16)
+(def patch 17)
 (def revision "alpha")
 (def name (str major "." minor "." patch "-" revision))
 
-(def date "2022-12-08")
+(def date "2022-12-09")
 
 (defn version
   ([ws-type]
