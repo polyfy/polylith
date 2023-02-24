286d1309c7901cb67de546583ce11c4d6a94c240
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-02-24 09:03:31 +0100

    Also show the non top namespace files in warning 205.

--- components/validator/src/polylith/clj/core/validator/m205_non_top_namespace.clj
+++ components/validator/src/polylith/clj/core/validator/m205_non_top_namespace.clj
@@ -1,20 +1,21 @@
 (ns polylith.clj.core.validator.m205-non-top-namespace
-  (:require [polylith.clj.core.util.interface :as util]
+  (:require [clojure.string :as str]
+            [polylith.clj.core.util.interface :as util]
             [polylith.clj.core.util.interface.color :as color]))
 
-(defn warning [{:keys [name type non-top-ns]} color-mode]
-  (let [message (str "Non top namespace " (color/namespc non-top-ns color-mode) " was found in " (color/brick type name color-mode) ".")]
+(defn warning [[[name type non-top-ns] file-maps] color-mode]
+  (let [files (str/join ", " (mapv :file file-maps))
+        message (str "Non top namespace " (color/namespc non-top-ns color-mode) " was found in " (color/brick type name color-mode) ", files: " (color/grey color-mode files))]
     (util/ordered-map :type "warning"
                       :code 205
                       :message (color/clean-colors message)
                       :colorized-message message)))
 
 (defn non-top-nss [{:keys [name type non-top-namespaces]}]
-  (mapv #(assoc (select-keys % [:non-top-ns]) :name name :type type)
+  (mapv #(assoc (select-keys % [:non-top-ns :file]) :name name :type type)
         non-top-namespaces))
 
 (defn warnings [components bases color-mode]
   (map #(warning % color-mode)
-       (sort-by (juxt :type :name)
-                (set (mapcat non-top-nss
-                             (concat components bases))))))
+       (group-by (juxt :name :type :non-top-ns) (mapcat non-top-nss
+                                                        (concat components bases)))))
