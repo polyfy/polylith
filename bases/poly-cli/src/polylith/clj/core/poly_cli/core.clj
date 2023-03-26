0b5c191645c7e3065ddb53ba81d51f945e033aaf
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-03-26 12:28:00 +0200

    Updated comment.

--- bases/poly-cli/src/polylith/clj/core/poly_cli/core.clj
+++ bases/poly-cli/src/polylith/clj/core/poly_cli/core.clj
@@ -39,6 +39,8 @@
 (comment
   (-main "test" ":all" ":no-exit")
   (-main "check" "ws-file:../sandbox/furkan.edn" ":no-exit")
+  (-main "ws" "get:components:account:namespaces:src:core:file-path" "ws-file:../sandbox/furkan.edn" ":no-exit")
+  (-main "ws" "get:components:api:namespaces:src:core:file-path" "ws-dir:../../Downloads/polylith-0.1.0-alpha9/" ":no-exit")
   (-main "check" ":no-exit")
   (-main "check" "ws-dir:examples/local-dep" ":no-exit")
   (-main "ws" "get:components:without-src:non-top-namespaces" "ws-dir:examples/local-dep" ":no-exit")
