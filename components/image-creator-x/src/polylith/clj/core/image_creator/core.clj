98e3831e1dd127138f3e8dd79887ecba17729d33
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-06-29 17:21:00 +0200

    Add support for JetBrains Mono Medium font.

--- components/image-creator-x/src/polylith/clj/core/image_creator/core.clj
+++ components/image-creator-x/src/polylith/clj/core/image_creator/core.clj
@@ -1,9 +1,10 @@
 (ns polylith.clj.core.image-creator.core
-  (:require [clojure.string :as str]
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
             [clojure2d.core :as c2d]
+            [polylith.clj.core.file.interface :as file]
             [polylith.clj.core.util.interface.color :as color]))
 
-(def font-name "Courier New")
 (def font-size 20)
 (def font-width 12) ;(c2d/char-width graphics \x)
 (def font-height 23) ; (c2d/font-height graphics))
@@ -22,9 +23,18 @@
     (when (-> text str/blank? not)
       (set-text-row! text x (+ y (* index font-height)) font-width graphics))))
 
+(def font-path "components/image-creator-x/resources/JetBrainsMono-Medium.ttf")
+
+(defn load-font []
+  (try
+    (c2d/load-font (file/file font-path))
+    (catch Exception _
+      (c2d/load-font (io/resource font-path)))))
+
 (defn create-image [filename table canvas-areas]
   (let [width (+ 25 (* font-width (apply max (mapv #(-> % color/clean-colors count) table))))
         height (+ 45 (* font-height (count table)))
+        font (load-font)
         canvas (c2d/canvas width height :high)
         graphics (c2d/make-graphics canvas)]
     (doseq [{:keys [x y w h]} canvas-areas]
@@ -32,8 +42,7 @@
        (c2d/rect graphics x y w h))
     (if (nil? canvas-areas)
       (c2d/set-background graphics 36 39 43))
-
-    (c2d/set-font graphics font-name)
+    (c2d/set-font graphics font)
     (c2d/set-font-attributes graphics font-size)
     (set-table! table 0 42 font-width font-height graphics)
     (c2d/save graphics filename)
