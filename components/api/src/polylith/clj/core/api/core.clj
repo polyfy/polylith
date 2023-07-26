8dcdc4935b8818d5e3225965927f334d633c2185
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-07-26 05:16:49 +0200

    Moved sections from install to sub sections.

--- components/api/src/polylith/clj/core/api/core.clj
+++ components/api/src/polylith/clj/core/api/core.clj
@@ -6,8 +6,7 @@
             [polylith.clj.core.workspace-clj.interface :as ws-clj]
             [polylith.clj.core.ws-explorer.interface :as ws-explorer]))
 
-(defn projects-to-deploy
-  [since]
+(defn projects-to-deploy [since]
   (let [since-then (str "since:" since)
         user-input (user-input/extract-params ["ws" since-then "skip:development"])
         workspace (-> user-input
@@ -22,14 +21,7 @@
     (keyword? key) (name key)
     :else key))
 
-(defn workspace
-  "Returns the workspace or part of the workspace by sending in either
-   a key that can be found in the :tag-patterns keys in workspace.edn,
-   optionally prefixed with 'previous-', or a git SHA, as the first argument,
-   and a list of keywords, strings, or numbers as the second argument.
-   :keys and :count are also valid keys to send in. If keys are empty,
-   returns the whole workspace."
-  [since keys]
+(defn workspace [since keys]
   (let [keys-str (map key->str keys)
         since-str (str "since:" (key->str since))
         args (if (empty? keys-str)
--- components/api/src/polylith/clj/core/api/interface.clj
+++ components/api/src/polylith/clj/core/api/interface.clj
@@ -9,5 +9,12 @@
   [since]
   (core/projects-to-deploy since))
 
-(defn workspace [stable-point & keys]
+(defn workspace
+  "Returns the workspace or part of the workspace by sending in either
+   a key that can be found in the :tag-patterns keys in workspace.edn,
+   optionally prefixed with 'previous-', or a git SHA, as the first argument,
+   and a list of keywords, strings, or numbers as the second argument.
+   :keys and :count are also valid keys to send in. If keys are empty,
+   returns the whole workspace."
+  [stable-point & keys]
   (core/workspace stable-point keys))
