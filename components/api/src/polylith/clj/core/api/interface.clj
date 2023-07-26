a4c7d71fcdc7be40c9b79c8cc33b61c7b403d3a0
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-07-26 04:31:53 +0200

    Moved docstrings from implementing namespace to the interface for the api component.

--- components/api/src/polylith/clj/core/api/interface.clj
+++ components/api/src/polylith/clj/core/api/interface.clj
@@ -2,7 +2,11 @@
   (:require [polylith.clj.core.api.core :as core])
   (:gen-class))
 
-(defn projects-to-deploy [since]
+(defn projects-to-deploy
+  "Returns the projects that have been affected since last deploy,
+   tagged in git following the pattern defined by :release-tag-pattern in
+   deps.edn, or v[0-9]* if not defined."
+  [since]
   (core/projects-to-deploy since))
 
 (defn workspace [stable-point & keys]
