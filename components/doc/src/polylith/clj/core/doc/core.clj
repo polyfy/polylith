353d637b7ff93c631344b3c3da075497e91356cd
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-08-15 09:29:56 +0100

    Support starting the shell in different ways + replaced the Parameters page with Maintain poly.

--- components/doc/src/polylith/clj/core/doc/core.clj
+++ components/doc/src/polylith/clj/core/doc/core.clj
@@ -3,29 +3,31 @@
             [polylith.clj.core.version.interface :as ver]
             [polylith.clj.core.doc.navigation.more :as more]))
 
-(defn doc-url [branch local?]
+(defn doc-url [branch local? github?]
   (cond
-    branch (if (= "master" branch)
+    github? (if (= "master" branch)
              (str "https://github.com/polyfy/polylith/tree/master")
              (str "https://github.com/polyfy/polylith/blob/" branch))
     local? (str "http://localhost:8000/d/polylith/clj-poly/" ver/name)
     :else (str "https://cljdoc.org/d/polylith/clj-poly/" ver/name)))
 
-(defn bookmark-url [page bookmark branch local?]
-  (str (doc-url branch local?) "/doc/reference/" page
-       (if branch ".adoc" "")
+(defn bookmark-url [page bookmark branch local? github?]
+  (str (doc-url branch local? github?)
+       (if github?
+         (str "/doc/" page ".adoc")
+         (str "/doc/reference/" page))
        "#" bookmark))
 
-(defn page-url [page branch local?]
-  (str (doc-url branch local?)
-       (if (and branch (= "readme" page))
+(defn page-url [page branch local? github?]
+  (str (doc-url branch local? github?)
+       (if (and github? (= "readme" page))
          "/" "/doc/")
        (or page "readme")
-       (if branch ".adoc" "")))
+       (if github? ".adoc" "")))
 
 (defn more-url [page local?]
   (or (get-in more/config (map keyword (conj page "url")))
-      (page-url "readme" nil local?)))
+      (page-url "readme" nil local? false)))
 
 (defn command
   "Helper function that makes sure that we go to the beginning of the help
@@ -35,13 +37,14 @@
     ""
     command))
 
-(defn open-doc [branch local? help more page ws unnamed-args]
-  (let [help (command "help" help unnamed-args)
+(defn open-doc [branch local? github? help more page ws unnamed-args]
+  (let [branch (or branch "master")
+        help (command "help" help unnamed-args)
         ws (command "ws" ws unnamed-args)
         url (cond
-              help (bookmark-url "commands" help branch local?)
+              help (bookmark-url "commands" help branch local? github?)
               more (more-url more local?)
-              page (page-url page branch local?)
-              ws (bookmark-url "workspace-structure" ws branch local?)
-              :else (page-url nil branch local?))]
+              page (page-url page branch local? github?)
+              ws (bookmark-url "workspace-structure" ws branch local? github?)
+              :else (page-url nil branch local? github?))]
     (browse/browse-url url)))
