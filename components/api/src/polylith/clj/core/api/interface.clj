af6690006c0f189a42611a82176263463b953ff4
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-08-16 07:23:24 +0100

    Updated comment in api.

--- components/api/src/polylith/clj/core/api/interface.clj
+++ components/api/src/polylith/clj/core/api/interface.clj
@@ -4,8 +4,8 @@
 
 (defn projects-to-deploy
   "Returns the projects that have been affected since last deploy,
-   tagged in git following the pattern defined by `:release-tag-pattern` in
-   `deps.edn`, or `v[0-9]*` if not defined.
+   tagged in git following the pattern defined by [[:release-tag-pattern]] in
+   [[deps.edn]], or [[v[0-9]*]] if not defined.
 
    Example:
      (projects-to-deploy nil)       ;; poly ws get:changes:changed-or-affected-projects skip:dev since:stable
@@ -15,10 +15,10 @@
 
 (defn workspace
   "Returns the workspace or part of the workspace by sending in either
-   a key that can be found in the `:tag-patterns` keys in `workspace.edn`,
-   optionally prefixed with 'previous-', or a git SHA, as the first argument,
+   a key that can be found in the [[:tag-patterns]] keys in [[workspace.edn]],
+   optionally prefixed with [[previous-]], or a git SHA, as the first argument,
    and a list of keywords, strings, or numbers as the second argument.
-   `:keys` and `:count` are also valid keys to send in. If keys are empty,
+   [[:keys]] and [[:count]] are also valid keys to send in. If keys are empty,
    returns the whole workspace.
 
    Example:
