217aca691e1684881e051aa19777f2d35ba1cb1d
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-08-17 09:06:07 +0100

    Updated poly-as-a-library.adoc.

--- components/api/src/polylith/clj/core/api/interface.clj
+++ components/api/src/polylith/clj/core/api/interface.clj
@@ -1,15 +1,19 @@
 (ns polylith.clj.core.api.interface
-  (:require [polylith.clj.core.api.core :as core])
+  (:require [polylith.clj.core.api.core :as core]
+            [polylith.clj.core.version.interface :as version])
   (:gen-class))
+
+(def api-version version/api-version)
+(def test-runner-api-version version/test-runner-api-version)
+(def ws-api-version version/ws-api-version)
 
 (defn projects-to-deploy
   "Returns the projects that have been affected since last deploy,
    tagged in git following the pattern defined by `:release-tag-pattern` in
    `deps.edn`, or `v[0-9]*` if not defined.
 
-   Example:
-     - (projects-to-deploy nil)       ;; `poly ws get:changes:changed-or-affected-projects skip:dev since:stable`
-     - (projects-to-deploy \"release\"  ;; `poly ws get:changes:changed-or-affected-projects skip:dev since:release)`"
+   Read more in the documentation on how to use this:
+     poly doc page:poly-as-a-library"
   [since]
   (core/projects-to-deploy since))
 
@@ -18,13 +22,10 @@
    a key that can be found in the `:tag-patterns` keys in `workspace.edn`,
    optionally prefixed with 'previous-', or a git SHA, as the first argument,
    and a list of keywords, strings, or numbers as the second argument.
-   `:keys` and `:count` are also valid keys to send in. If keys are empty,
-   returns the whole workspace.
+   `:keys` and `:count` are also valid keys to send in.
 
-   Example:
-     - (workspace nil)                             ;; `poly ws`
-     - (workspace \"release\")                       ;; `poly ws since:release`
-     - (workspace nil \"settings\" \"interface-ns\")  ;; `poly ws get:settings:interface-ns`"
+   Read more in the documentation on how to use this:
+     poly doc page:poly-as-a-library"
   [stable-point & keys]
   (core/workspace stable-point keys))
 
@@ -33,5 +34,5 @@
   (projects-to-deploy "release")
   (workspace nil)
   (workspace "release")
-  (workspace "stable" "settings" "interface-ns")  ;;
+  (workspace "stable" "settings" "interface-ns")
   #__)
