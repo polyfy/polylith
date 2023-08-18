437e8119a21722622408b6a6c5fd056d24d69285
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-08-18 17:09:07 +0100

    Updated text that is shown when creating a brick. Use {cljdoc-doc-url}in the introduction link in readme.adoc.

--- components/creator/src/polylith/clj/core/creator/brick.clj
+++ components/creator/src/polylith/clj/core/creator/brick.clj
@@ -5,7 +5,7 @@
             [clojure.string :as str]))
 
 (def create-brick-message
-  (str "  Remember to add paths and/or local/root dependency to dev and project 'deps.edn' files."))
+  (str "  Remember to add :local/root dependencies to dev and project 'deps.edn' files."))
 
 (defn create-config-file [ws-dir bricks-dir brick-name is-git-add]
   (let [config-filename (str ws-dir "/" bricks-dir "/" brick-name "/deps.edn")]
