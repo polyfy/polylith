8f7320dea09cac5459469422da133e983b8705c1
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-08-18 19:08:48 +0100

    Changed to a generic link for to the poly tool documentation, in the 'create workspace' command.

--- components/creator/test/polylith/clj/core/creator/workspace_test.clj
+++ components/creator/test/polylith/clj/core/creator/workspace_test.clj
@@ -99,7 +99,7 @@
             "The Polylith documentation can be found here:"
             ""
             "- The [high-level documentation](https://polylith.gitbook.io/polylith)"
-            "- The [poly tool documentation](https://cljdoc.org/d/polylith/clj-poly/0.2.18/doc/readme)"
+            "- The [poly tool documentation](https://cljdoc.org/d/polylith/clj-poly/CURRENT)"
             "- The [RealWorld example app documentation](https://github.com/furkan3ayraktar/clojure-polylith-realworld-example-app)"
             ""
             "You can also get in touch with the Polylith Team on [Slack](https://clojurians.slack.com/archives/C013B7MQHJQ)."
