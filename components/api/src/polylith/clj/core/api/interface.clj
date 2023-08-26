9485f4b2eb5919bd4cb8784b203a9aa63e47105f
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-08-26 07:29:29 +0200

    Improve the api doc.

--- components/api/src/polylith/clj/core/api/interface.clj
+++ components/api/src/polylith/clj/core/api/interface.clj
@@ -1,10 +1,14 @@
 (ns polylith.clj.core.api.interface
+  "Read more by executing
+   ```clojure
+     poly doc page:poly-as-a-library
+   ```"
   (:require [polylith.clj.core.api.core :as core]
             [polylith.clj.core.version.interface :as version])
   (:gen-class))
 
 (def api-version
-  "The version of the different types of APIs in a hash map with the keys:
+  "The version of the different types of APIs, stored in a hash map with the keys:
    `:api`, `:test-runner`, and `:ws`"
   {:api version/api-version
    :test-runner version/test-runner-api-version
@@ -13,10 +17,7 @@
 (defn projects-to-deploy
   "Returns the projects that have been affected since last deploy,
    tagged in git following the pattern defined by `:release-tag-pattern` in
-   `deps.edn`, or `v[0-9]*` if not defined.
-
-   Read more in the documentation on how to use this by executing
-     `poly doc page:poly-as-a-library`"
+   `./deps.edn`, or `v[0-9]*` if not defined."
   [since]
   (core/projects-to-deploy since))
 
@@ -25,10 +26,7 @@
    a key that can be found in the `:tag-patterns` keys in `workspace.edn`,
    optionally prefixed with `previous-`, or a git SHA, as the first argument,
    and a list of keywords, strings, or numbers as the second argument.
-   `:keys` and `:count` are also valid keys to pass in.
-
-   Read more in the documentation on how to use this by executing
-     `poly doc page:poly-as-a-library`"
+   `:keys` and `:count` are also valid keys to pass in."
   [stable-point & keys]
   (core/workspace stable-point keys))
 
