d0b48beecb9bb10598fabbac54f0c9252ef40a34
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-11-09 19:47:32 +0100

    Change warning 206 to error 111.

--- components/validator/src/polylith/clj/core/validator/core.clj
+++ components/validator/src/polylith/clj/core/validator/core.clj
@@ -10,11 +10,11 @@
             [polylith.clj.core.validator.m108-project-with-multi-implementing-component :as m108]
             [polylith.clj.core.validator.m109-invalid-test-runner-constructor :as m109]
             [polylith.clj.core.validator.m110-invalid-config-file :as m110]
+            [polylith.clj.core.validator.m111-unreadable-namespace :as m111]
             [polylith.clj.core.validator.m201-mismatching-argument-lists :as m201]
             [polylith.clj.core.validator.m202-missing-paths :as m202]
             [polylith.clj.core.validator.m203-path-exists-in-both-dev-and-profile :as m203]
             [polylith.clj.core.validator.m205-non-top-namespace :as m205]
-            [polylith.clj.core.validator.m206-unreadable-namespace :as m206]
             [polylith.clj.core.validator.m207-unnecessary-components-in-project :as m207]))
 
 (defn has-errors? [messages]
@@ -33,11 +33,11 @@
         (m108/errors interfaces projects paths color-mode)
         (m109/errors settings color-mode)
         (m110/errors config-errors)
+        (m111/errors components bases projects color-mode)
         (m201/warnings interfaces components color-mode)
         (m202/warnings projects paths color-mode)
         (m203/warnings settings projects color-mode)
         (m205/warnings components bases color-mode)
-        (m206/warnings components bases projects color-mode)
         (m207/warnings cmd settings projects is-dev color-mode)]
        (into #{} cat)
        (sort-by (juxt :type :code :message))
