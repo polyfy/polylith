9e79264981b0c5be6e6cb70c93a540a82c489510
Joakim Tengstrand <joakimtengstrand@gmail.com>
2023-12-15 22:31:23 +0100

    Add instructions on how to run tests from the root module in Cursive. (#417)
    Co-authored-by: Joakim Tengstrand <joakimtengstrand@gmai.com>

--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -25,7 +25,7 @@
 (def minor 2)
 (def patch 18)
 (def revision SNAPSHOT) ;; Set to SNAPSHOT or RELEASE.
-(def snapshot 22) ;; Increase by one for every snapshot release, or set to 0 if a release.
+(def snapshot 23) ;; Increase by one for every snapshot release, or set to 0 if a release.
 (def snapshot? (= SNAPSHOT revision))
 
 (def name-without-rev (str major "." minor "." patch))
