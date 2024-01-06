536c66f0d0c0eba1e3cde5693f74a273ab728c01
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-01-06 08:03:31 +0100

    Refactored code so that it can be reused.

--- components/ws-file/src/polylith/clj/core/ws_file/from_2_to_3/move_keep_lib_versions.clj
+++ components/ws-file/src/polylith/clj/core/ws_file/from_2_to_3/move_keep_lib_versions.clj
@@ -1,18 +1,17 @@
 (ns ^:no-doc polylith.clj.core.ws-file.from-2-to-3.move-keep-lib-versions
   (:require [polylith.clj.core.common.interface :as common]))
 
-(defn move-keep-lib-versions [entities name->settings]
-  (reduce-kv (fn [entities entity-name {:keys [keep-lib-versions]}]
-               (let [index (common/find-entity-index entity-name entities)]
+(defn move-project-key [key entities name->settings]
+  (reduce-kv (fn [entities entity-name project-settings]
+               (let [index (common/find-entity-index entity-name entities)
+                     value (key project-settings)]
                  (cond-> entities
-                         (and index
-                              keep-lib-versions) (assoc-in [index :keep-lib-versions]
-                                                           keep-lib-versions))))
+                         (and index value) (assoc-in [index key] value))))
              entities name->settings))
 
 (defn convert [{:keys [bases components projects settings] :as workspace}]
   (let [bricks-settings (:bricks settings)
         projects-settings (:projects settings)]
-    (assoc workspace :bases (move-keep-lib-versions bases bricks-settings)
-                     :components (move-keep-lib-versions components bricks-settings)
-                     :projects (move-keep-lib-versions projects projects-settings))))
+    (assoc workspace :bases (move-project-key :keep-lib-versions bases bricks-settings)
+                     :components (move-project-key :keep-lib-versions components bricks-settings)
+                     :projects (move-project-key :keep-lib-versions projects projects-settings))))
