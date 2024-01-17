8a9fcc495a63528c91428df20cad05b1136293f9
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-01-17 17:53:47 +0100

    Convert project settings from :settings to :projects/PROJECT when reading old exported files.

--- components/ws-file/src/polylith/clj/core/ws_file/from_2_to_3/converter.clj
+++ components/ws-file/src/polylith/clj/core/ws_file/from_2_to_3/converter.clj
@@ -1,25 +1,8 @@
-(ns ^:no-doc polylith.clj.core.ws-file.from-2-to-3.converter
-  (:require [polylith.clj.core.common.interface :as common]))
+(ns polylith.clj.core.ws-file.from-2-to-3.converter
+  (:require [polylith.clj.core.ws-file.from-2-to-3.settings :as settings]
+            [polylith.clj.core.ws-file.from-2-to-3.changes :as changes]))
 
-(defn move-settings-key-to-entity [entities key name->settings]
-  (reduce-kv
-    (fn [entities entity-name project-settings]
-      (let [index (common/find-entity-index entity-name entities)
-            value (key project-settings)]
-        (cond-> entities
-                (and index value) (assoc-in [index key] value))))
-    entities name->settings))
-
-(defn convert-projects [projects projects-settings]
-  (-> projects
-      (move-settings-key-to-entity :test projects-settings)
-      (move-settings-key-to-entity :necessary projects-settings)
-      (move-settings-key-to-entity :keep-lib-versions projects-settings)))
-
-(defn convert [{:keys [bases components projects settings] :as workspace}]
-  (let [bricks-settings (:bricks settings)
-        projects-settings (:projects settings)]
-    (assoc workspace :bases (move-settings-key-to-entity bases :keep-lib-versions bricks-settings)
-                     :components (move-settings-key-to-entity components :keep-lib-versions bricks-settings)
-                     :projects (convert-projects projects projects-settings)
-                     :settings (dissoc settings :bases :projects))))
+(defn convert [workspace]
+  (-> workspace
+      (settings/convert)
+      (changes/convert)))
