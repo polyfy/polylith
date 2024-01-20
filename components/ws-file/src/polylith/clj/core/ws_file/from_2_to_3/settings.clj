41c2687378da651cd84b13fe0c0724b1e8be7bfe
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-01-20 09:31:53 +0100

    Migrate :profiles when reading old exported workspaces.

--- components/ws-file/src/polylith/clj/core/ws_file/from_2_to_3/settings.clj
+++ components/ws-file/src/polylith/clj/core/ws_file/from_2_to_3/settings.clj
@@ -16,14 +16,15 @@
       (move-settings-key-to-entity :necessary projects-settings)
       (move-settings-key-to-entity :keep-lib-versions projects-settings)))
 
+(defn profile [[profile-name settings]]
+  (assoc settings :name profile-name
+                  :type "profile"))
+
 (defn convert [{:keys [bases components projects settings] :as workspace}]
   (let [bricks-settings (:bricks settings)
         projects-settings (:projects settings)]
     (assoc workspace :bases (move-settings-key-to-entity bases :keep-lib-versions bricks-settings)
                      :components (move-settings-key-to-entity components :keep-lib-versions bricks-settings)
                      :projects (convert-projects projects projects-settings)
-                     :settings (dissoc settings :bases :projects))))
-
-(comment
-  (def workspace dev.jocke/workspace)
-  #__)
+                     :profiles (mapv profile (:profile-to-settings settings))
+                     :settings (dissoc settings :bases :projects :profile-to-settings))))
