3f2e128fc28ab6b064812b236cea8688c797bf83
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-01-28 07:05:26 +0100

    Cleaned upp dev namespace.

--- development/src/dev/dev_common.clj
+++ development/src/dev/dev_common.clj
@@ -2,10 +2,15 @@
   (:require [polylith.clj.core.command.interface :as command]
             [polylith.clj.core.user-input.interface :as user-input]))
 
-(defn ws-from-file [filename]
-  (let [input (user-input/extract-arguments ["info" (str "ws-file:" filename)])]
+(defn ws-from-file [command filename]
+  (let [input (user-input/extract-arguments [command (str "ws-file:" filename)])]
     (command/read-workspace filename input)))
 
 (defn dir [ws-dir & args]
   (user-input/extract-arguments (concat ["info" (str "ws-dir:" ws-dir) "replace:/Users/joakimtengstrand:USER-HOME"]
                                         args)))
+
+(defn execute [command ws-dir & args]
+  (-> (user-input/extract-arguments (concat [command (str "ws-dir:" ws-dir) "replace:/Users/joakimtengstrand:USER-HOME"]
+                                            args))
+      (command/execute-command)))
