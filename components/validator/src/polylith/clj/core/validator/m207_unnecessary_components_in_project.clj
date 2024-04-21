ded189731c6cc3cfedf2b21206d9dbefa05c021b
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-04-21 07:09:02 +0200

    Don't show warning 207 "Unnecessary components..." for external components.

--- components/validator/src/polylith/clj/core/validator/m207_unnecessary_components_in_project.clj
+++ components/validator/src/polylith/clj/core/validator/m207_unnecessary_components_in_project.clj
@@ -10,20 +10,25 @@
           (:direct test)
           (:indirect test)))
 
+(defn exclude-external [components]
+  (sort (filter #(not (str/includes? % "/"))
+                components)))
+
 (defn warning [{:keys [is-dev name necessary component-names deps]} check-dev color-mode]
   "Warns if a component is not used by any brick in the project for all
    project except development (which can be included by passing in :dev).
-   Components can be excluded from the check by putting it in the :necessary
+   Components can be excluded from the check by putting them in the :necessary
    vector for a project in :projects in workspace.edn."
   (let [{:keys [src test]} component-names
         defined-components (set/difference (set (concat src test)) (set necessary))
         used-components (set (mapcat component-deps deps))
-        unused-components (str/join ", " (map #(color/component % color-mode)
-                                              (sort (set/difference defined-components used-components))))]
+        unused-components (exclude-external (set/difference defined-components used-components))
+        unused-components-str (str/join ", " (map #(color/component % color-mode)
+                                                  unused-components))]
     (when (and (or check-dev (not is-dev))
-               (seq unused-components))
+               (seq unused-components-str))
       (let [message (str "Unnecessary components were found in the " (color/project name color-mode)
-                         " project and may be removed: " unused-components ". To ignore this warning,"
+                         " project and may be removed: " unused-components-str ". To ignore this warning,"
                          " execute 'poly help check' and follow the instructions for warning 207.")]
         [(util/ordered-map :type "warning"
                            :code 207
