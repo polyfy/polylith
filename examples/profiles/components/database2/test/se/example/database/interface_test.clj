855d146c1d5d7f39000cf432ba76a78a98ec8a5b
Timo Kramer <4785848+TimoKramer@users.noreply.github.com>
2024-06-22 08:32:06 +0200

    profile alias test (#472)

---
+++ examples/profiles/components/database2/test/se/example/database/interface_test.clj
@@ -0,0 +1,6 @@
+(ns se.example.database2.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [se.example.database2.interface :as database2]))
+
+(deftest dummy-test
+  (is (= 1 1)))
