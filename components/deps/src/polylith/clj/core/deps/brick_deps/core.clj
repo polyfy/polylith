d36c22871588c513246a317e699e3a77883ec6f7
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-06-22 21:01:15 +0200

    Calculate illegal namespace dependencies correctly (will show error 101 otherwise).

--- components/deps/src/polylith/clj/core/deps/brick_deps/core.clj
+++ components/deps/src/polylith/clj/core/deps/brick_deps/core.clj
@@ -2,50 +2,63 @@
   (:require [clojure.set :as set]
             [polylith.clj.core.common.interface :as common]))
 
-(defn brick-dep [brick-id from-ns suffixed-top-ns base-names imported-ns]
+(defn brick-dep [brick-id from-type from-ns suffixed-top-ns interface-names base-names imported-ns]
   (let [{:keys [root-ns depends-on-ns]} (common/extract-namespace suffixed-top-ns imported-ns)
-        type (if (contains? base-names root-ns) "base" "component")]
+        type (cond
+               (contains? base-names root-ns) "base"
+               (contains? interface-names root-ns) "interface"
+               :library "library")]
     (when (and root-ns
                (not= brick-id root-ns))
-      {:to-brick-id root-ns
+      {:from-type from-type
        :from-ns from-ns
        :to-type type
+       :to-brick-id root-ns
        :to-namespace depends-on-ns})))
 
-(defn brick-ns-import-deps [brick-id suffixed-top-ns base-names {:keys [name imports]}]
-  (map #(brick-dep brick-id name suffixed-top-ns base-names (str %))
+(defn brick-ns-import-deps [brick-id type suffixed-top-ns interface-names base-names {:keys [name imports]}]
+  (map #(brick-dep brick-id type name suffixed-top-ns interface-names base-names (str %))
        imports))
 
-(defn brick-ns-deps [brick-id suffixed-top-ns base-names namespaces]
-  (vec (mapcat #(brick-ns-import-deps brick-id suffixed-top-ns base-names %)
+(defn brick-ns-deps [brick-id type suffixed-top-ns interface-names base-names namespaces]
+  (vec (mapcat #(brick-ns-import-deps brick-id type suffixed-top-ns interface-names base-names %)
                namespaces)))
 
-(defn brick-deps-for-source [suffixed-top-ns {:keys [name interface namespaces]} base-names source]
+(defn brick-deps-for-source [suffixed-top-ns {:keys [name type interface namespaces]} interface-names base-names source]
   (let [brick-id (or (:name interface) name)]
     (filter :to-brick-id
-            (brick-ns-deps brick-id suffixed-top-ns base-names (source namespaces)))))
+            (brick-ns-deps brick-id type suffixed-top-ns interface-names base-names (source namespaces)))))
 
-(defn valid-interface? [{:keys [to-brick-id to-namespace]} interface-names interface-ns]
-  (and (contains? interface-names to-brick-id)
-       (common/interface-ns? to-namespace interface-ns)))
+(defn interface-dep? [{:keys [to-type to-namespace]} interface-ns source]
+  (and (= "interface" to-type)
+       (or (= :test source)
+           (common/interface-ns? to-namespace interface-ns))))
 
-(defn deps-for-source [suffixed-top-ns interface-names base-names {:keys [type] :as brick} interface-ns source]
-  (let [deps (brick-deps-for-source suffixed-top-ns brick base-names source)
-        dep-ids (set (map :to-brick-id deps))
-        interface-deps (if (= :test source)
-                         (set/intersection dep-ids interface-names)
-                         (set (map :to-brick-id
-                                   (filter #(valid-interface? % interface-names interface-ns)
-                                           deps))))
-        base-deps (set/intersection dep-ids base-names)
-        valid-deps (if (= "component" type)
-                     interface-deps
-                     (set/union interface-deps base-deps))
-        illegal-deps (filterv #(not (contains? valid-deps (:to-brick-id %)))
-                              deps)]
-    [(vec (sort interface-deps))
-     (vec (sort base-deps))
-     illegal-deps]))
+(defn illegal-dep? [{:keys [from-type to-type to-namespace]} interface-ns source]
+  (or (and (= "component" from-type)
+           (= "base" to-type)
+           (= :src source))
+      (and (= "interface" to-type)
+           (= :src source)
+           (not (common/interface-ns? to-namespace interface-ns)))))
+
+(defn base? [{:keys [to-type]}]
+  (= "base" to-type))
+
+(defn unique-ids [deps]
+  (vec (sort (set deps))))
+
+(defn deps-for-source [suffixed-top-ns interface-names base-names brick interface-ns source]
+  (let [deps (brick-deps-for-source suffixed-top-ns brick interface-names base-names source)
+        interface-deps (map :to-brick-id
+                            (filter #(interface-dep? % interface-ns source)
+                                    deps))
+        base-deps (set/intersection (map :to-brick-id
+                                         (filter base? deps)))
+        illegal-deps (filter #(illegal-dep? % interface-ns source) deps)]
+    [(unique-ids interface-deps)
+     (unique-ids base-deps)
+     (unique-ids illegal-deps)]))
 
 (defn brick-deps
   "Returns the interface and base dependencies for a brick (component or base)."
