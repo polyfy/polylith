72b405e1259a63723b0a1590e317227bf28b09ff
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-06-23 18:44:27 +0200

    Formatting.

--- projects/poly/test/project/poly/packaged_dependencies_test.clj
+++ projects/poly/test/project/poly/packaged_dependencies_test.clj
@@ -1,9 +1,8 @@
 (ns project.poly.packaged-dependencies-test
-  (:require
-   [clojure.test :refer :all]
-   [polylith.clj.core.user-input.interface :as user-input]
-   [polylith.clj.core.workspace.interface :as workspace]
-   [polylith.clj.core.ws-explorer.interface :as ws-explorer]))
+  (:require [clojure.test :refer :all]
+            [polylith.clj.core.user-input.interface :as user-input]
+            [polylith.clj.core.workspace.interface :as workspace]
+            [polylith.clj.core.ws-explorer.interface :as ws-explorer]))
 
 (defn workspace [& args]
   (-> (user-input/extract-arguments (concat ["info" (str "ws-dir:.") "color-mode:none"] args))
