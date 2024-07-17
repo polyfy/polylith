ac6845812f43e6f694451b22d960ea2149408de4
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-07-17 04:49:00 +0200

    Preparing 0.3.21-SNAPSHOT # 1

--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -22,10 +22,10 @@
 (def SNAPSHOT "SNAPSHOT")
 
 (def major 0)
-(def minor 2)
-(def patch 20)
-(def revision RELEASE) ;; Set to SNAPSHOT or RELEASE.
-(def snapshot 0) ;; Increase by one for every snapshot release, or set to 0 if a release.
+(def minor 3)
+(def patch 21)
+(def revision SNAPSHOT) ;; Set to SNAPSHOT or RELEASE.
+(def snapshot 1) ;; Increase by one for every snapshot release, or set to 0 if a release.
                   ;; Also update :snapshot-version: at the top of readme.adoc.
 (def snapshot? (= SNAPSHOT revision))
 
