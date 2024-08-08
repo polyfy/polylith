adfcaa2789acad5d29a5d47ffeaeefd19672250a
Joakim Tengstrand <joakimtengstrand@gmail.com>
2024-08-08 13:40:12 +0200

    Show correct Integrant example system image (#503)
    * Show correct image under Integrant system.
    * 0.3.21-SNAPSHOT # 4

--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -25,7 +25,7 @@
 (def minor 3)
 (def patch 21)
 (def revision SNAPSHOT) ;; Set to SNAPSHOT or RELEASE.
-(def snapshot 3) ;; Increase by one for every snapshot release, or set to 0 if a release.
+(def snapshot 4) ;; Increase by one for every snapshot release, or set to 0 if a release.
                   ;; Also update :snapshot-version: at the top of readme.adoc.
 (def snapshot? (= SNAPSHOT revision))
 
