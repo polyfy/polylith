c078546bc0d26f4301d0c4efeebabc9e09da005e
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-08-08 12:03:03 +0200

    Removed examples/integrant-system (hosted here: https://github.com/marksto/polylith-integrant).

--- examples/integrant-system/components/config/src/integrant/config/interface.clj
+++
@@ -1,9 +0,0 @@
-(ns
-  ^{:doc    "An abstract system configuration as a Polylith component"
-    :author "Mark Sto"}
-  integrant.config.interface
-  (:require [integrant.config.core :as core]))
-
-(defn load-config
-  []
-  (core/load-config))
--- examples/integrant-system/components/pg-ops/test/integrant/pg_ops/interface_test.clj
+++
@@ -1,6 +0,0 @@
-(ns integrant.pg-ops.interface-test
-  (:require [clojure.test :as test :refer :all]
-            [integrant.pg-ops.interface :as pg-ops]))
-
-(deftest dummy-test
-  (is (= 1 1)))
