e7bf2baa99a8ba8afcd85397635e5a4ba191cdd3
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-09-29 12:10:19 +0200

    0.2.22-SNAPSHOT # 2

--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -37,7 +37,7 @@
 
 (def tool (if system/extended? "polyx" "poly"))
 
-(def date "2024-09-06")
+(def date "2024-09-29")
 
 ;; Execute 'poly doc page:versions' to see when different changes was introduced.
 (def api-version {:breaking 1, :non-breaking 0})
