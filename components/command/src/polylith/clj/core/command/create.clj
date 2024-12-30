16ff823d241c7dcf404a51a36d132a665a985c79
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-12-30 22:42:34 +0100

    Pass in dialect to Selmer.

--- components/command/src/polylith/clj/core/command/create.clj
+++ components/command/src/polylith/clj/core/command/create.clj
@@ -8,7 +8,7 @@
                 "Please use 'create " (common/entity->long entity-short) "' instead.")))
 
 (defn create [current-dir workspace
-              [_ entity] name alias top-ns interface branch git-add? commit? color-mode]
+              [_ entity] name alias dialect top-ns interface branch git-add? commit? color-mode]
   (let [ent (common/entity->short entity)
         git-add (if (-> git-add? nil? not)
                   git-add?
@@ -16,8 +16,8 @@
     (if (contains? #{"b" "c" "p" "w"} entity)
       (print-warning entity color-mode))
     (condp = ent
-      "w" (creator/create-workspace current-dir name top-ns branch commit?)
-      "p" (when (= :ok (creator/create-project workspace name git-add))
+      "w" (creator/create-workspace current-dir name top-ns dialect branch commit?)
+      "p" (when (= :ok (creator/create-project workspace name dialect git-add))
             (creator/print-alias-message name alias color-mode))
-      "b" (creator/create-base workspace name git-add)
-      "c" (creator/create-component workspace name interface git-add))))
+      "b" (creator/create-base workspace name dialect git-add)
+      "c" (creator/create-component workspace name interface dialect git-add))))
--- components/creator/src/polylith/clj/core/creator/base.clj
+++ components/creator/src/polylith/clj/core/creator/base.clj
@@ -1,15 +1,15 @@
 (ns ^:no-doc polylith.clj.core.creator.base
   (:require [polylith.clj.core.creator.brick :as brick]))
 
-(defn create-base [ws-dir settings base-name is-git-add]
+(defn create-base [ws-dir settings base-name dialect is-git-add]
   (let [{:keys [top-namespace]} settings
         bases-dir (str ws-dir "/bases/" base-name)]
     (brick/create-resources-dir ws-dir "bases" base-name is-git-add)
     (brick/create-config-file ws-dir "bases" base-name is-git-add)
-    (brick/create-src-ns ws-dir "bases" top-namespace bases-dir "core" base-name is-git-add)
-    (brick/create-test-ns ws-dir "bases" top-namespace bases-dir "core" base-name "core" is-git-add)))
+    (brick/create-src-ns ws-dir "bases" top-namespace bases-dir "core" base-name dialect is-git-add)
+    (brick/create-test-ns ws-dir "bases" top-namespace bases-dir "core" base-name "core" dialect is-git-add)))
 
-(defn create [{:keys [ws-dir settings] :as workspace} base-name is-git-add]
+(defn create [{:keys [ws-dir settings] :as workspace} base-name dialect is-git-add]
   (brick/create-brick workspace
                       base-name
-                      (fn [] (create-base ws-dir settings base-name is-git-add))))
+                      (fn [] (create-base ws-dir settings base-name dialect is-git-add))))
