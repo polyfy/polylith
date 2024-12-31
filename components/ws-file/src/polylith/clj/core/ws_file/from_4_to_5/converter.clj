2b1a90e8ff17b7e425c2365102806e2774afe4ce
Joakim Tengstrand <joakimtengstrand@gmai.com>
2024-12-31 08:36:44 +0100

    Exclude namespaces from the public API.

--- components/ws-file/src/polylith/clj/core/ws_file/from_4_to_5/converter.clj
+++ components/ws-file/src/polylith/clj/core/ws_file/from_4_to_5/converter.clj
@@ -1,4 +1,4 @@
-(ns polylith.clj.core.ws-file.from-4-to-5.converter)
+(ns ^:no-doc polylith.clj.core.ws-file.from-4-to-5.converter)
 
 (defn convert [workspace]
   (assoc workspace :ws-dialects #{"clj"}))
