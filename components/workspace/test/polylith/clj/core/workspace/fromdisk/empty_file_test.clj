a7e3a472b1d68cb820357724e6a71e2f36480ebe
Jeroen van Dijk <noreply.git.exciting924@passmail.net>
2025-05-28 15:08:30 +0300

    Fix #523: handle empty namespaces

---
+++ components/workspace/test/polylith/clj/core/workspace/fromdisk/empty_file_test.clj
@@ -0,0 +1,69 @@
+(ns polylith.clj.core.workspace.fromdisk.empty-file-test
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [me.raynes.fs :as fs]
+            [polylith.clj.core.file.interface :as file]
+            [polylith.clj.core.workspace.fromdisk.namespaces-from-disk :as from-disk]))
+
+(deftest empty-file--namespace-extraction--should-handle-error-with-path-info
+  (let [temp-dir (str (fs/temp-dir "poly-test"))
+        empty-file-path (str temp-dir "/empty_component.clj")]
+    (try
+      ;; Create an empty file
+      (spit empty-file-path "")
+      
+      ;; Attempt to process the file - this should not throw an unhandled exception
+      ;; but instead return a properly formatted error
+      (let [namespace (from-disk/->namespace
+                        temp-dir 
+                        temp-dir 
+                        "test.core." 
+                        "interface" 
+                        empty-file-path)]
+        
+        ;; The namespace extraction should mark the file as invalid
+        ;; but not fail entirely with an unhandled exception
+        (is (map? namespace) "Should return a namespace map, not throw an exception")
+        (is (:is-invalid namespace) "Empty file should be marked as invalid")
+        (is (= "" (:namespace namespace)) "Empty file should have empty namespace string")
+        (is (str/ends-with? empty-file-path (:file-path namespace)) "File path should be preserved")
+        
+        ;; Test the underlying file/read-file function directly
+        (let [content (file/read-file empty-file-path)]
+          (is (= content :polylith.clj.core.file.interface/empty-file) "Empty file should be handled gracefully")))
+      
+      (finally
+        ;; Clean up
+        (fs/delete-dir temp-dir)))))
+
+(deftest real-file-loading--empty-file--should-return-error-with-file-path
+  (let [temp-dir (str (fs/temp-dir "poly-test"))
+        empty-file-path (str temp-dir "/src/test/core.clj")]
+    (try
+      ;; Create directory structure
+      (fs/mkdirs (str temp-dir "/src/test"))
+      ;; Create an empty file
+      (spit empty-file-path "")
+      
+      ;; Try to load namespaces from this directory structure
+      (let [namespaces (from-disk/namespaces-from-disk
+                         temp-dir
+                         [(str temp-dir "/src")]
+                         []
+                         "test."
+                         "interface")]
+        
+        ;; The namespace extraction should succeed but mark the file as invalid
+        (is (map? namespaces) "Should return a namespaces map")
+        (is (vector? (:src namespaces)) "Should have src vector")
+        (is (= 1 (count (:src namespaces))) "Should have one namespace")
+        
+        (let [ns-info (first (:src namespaces))]
+          (is (:is-invalid ns-info) "Empty file namespace should be marked as invalid")
+          (is (contains? ns-info :file-path) "Should contain file path")
+          (is (not (nil? (:file-path ns-info))) "File path should not be nil")))
+      
+      (finally
+        ;; Clean up
+        (fs/delete-dir temp-dir)))))
