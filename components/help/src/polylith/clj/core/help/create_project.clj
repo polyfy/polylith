67775b4a7b2ebdc93dbc94fc52c896702b0cb0a7
Joakim Tengstrand <joakimtengstrand@gmail.com>
2025-09-11 12:10:59 +0200

    Tell when package.json is created.

--- components/help/src/polylith/clj/core/help/create_project.clj
+++ components/help/src/polylith/clj/core/help/create_project.clj
@@ -8,6 +8,7 @@
        "    " (s/key "NAME" cm) " = The name of the project to create.\n"
        "\n"
        "    " (s/key "DIALECT" cm) " = The value is sent to Selmer when the project is created.\n"
+       "              If 'cljs' is provided, a 'package.json' file is also created.\n"
        "              Valid values are: clj, cljs\n"
        "\n"
        "    " (s/key ":git-add" cm) " = If " (s/key ":vcs" cm) " > " (s/key ":auto-add" cm) " in workspace.edn is set to false,\n"
