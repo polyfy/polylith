78099613388e2d7c4646afba244f2c6caed3048f
Joakim Tengstrand <joakimtengstrand@gmail.com>
2025-09-18 12:24:52 +0200

    Make sure to realize the path we show when running tests with :verbose.

--- components/test-runner-orchestrator/src/polylith/clj/core/test_runner_orchestrator/core.clj
+++ components/test-runner-orchestrator/src/polylith/clj/core/test_runner_orchestrator/core.clj
@@ -137,8 +137,8 @@
               create-test-runner)]
     (when (seq test-runners-seeing-test-sources)
       (let [lib-paths (resolve-deps project settings is-verbose color-mode)
-            all-paths (map str-util/ensure-slash
-                           (into [] cat [(:src paths) (:test paths) lib-paths]))
+            all-paths (mapv str-util/ensure-slash
+                            (into [] cat [(:src paths) (:test paths) lib-paths]))
             class-loader-delay (delay (common/create-class-loader all-paths color-mode))]
         (when is-verbose (println (str "# paths:\n" all-paths "\n")))
         (doseq [current-test-runner test-runners-seeing-test-sources]
