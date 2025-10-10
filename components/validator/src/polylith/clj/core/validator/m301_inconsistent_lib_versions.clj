e1d362eb4a7be57e3814f49f6c6761740bd7a840
Joakim Tengstrand <joakimtengstrand@gmail.com>
2025-10-10 15:20:36 +0200

    Added a check.

--- components/validator/src/polylith/clj/core/validator/m301_inconsistent_lib_versions.clj
+++ components/validator/src/polylith/clj/core/validator/m301_inconsistent_lib_versions.clj
@@ -6,7 +6,8 @@
 (defn library-message [[lib-name libraries] type]
   (let [versions (str/join ", " (mapv :version libraries))
         message (str "Inconsistent versions detected for library '" lib-name "': " versions)]
-    (when (not-empty libraries)
+    (when (and (keyword? type)
+               (not-empty libraries))
       [(util/ordered-map :type (name type)
                          :code 301
                          :message (color/clean-colors message)
