7a513ff0cc76a5da35810e0435c962c3437eaf07
Joakim Tengstrand <joakimtengstrand@gmail.com>
2025-11-05 19:45:05 +0100

    Add the file function, used from the dev.jocke namespace.

--- development/src/dev/dev_common.clj
+++ development/src/dev/dev_common.clj
@@ -2,6 +2,10 @@
   (:require [polylith.clj.core.command.interface :as command]
             [polylith.clj.core.user-input.interface :as user-input]))
 
+(defn file [ws-file & args]
+  (user-input/extract-arguments (concat ["info" (str "ws-file:" ws-file) "replace:/Users/joakimtengstrand:USER-HOME"]
+                                        args)))
+
 (defn dir [ws-dir & args]
   (user-input/extract-arguments (concat ["info" (str "ws-dir:" ws-dir) "replace:/Users/joakimtengstrand:USER-HOME"]
                                         args)))
