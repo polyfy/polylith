58f10c3f5d2da37d5a26fcf8d1eb8106453c27be
Joakim Tengstrand <joakimtengstrand@gmail.com>
2025-11-06 09:57:20 +0100

    Fix test

--- components/util/test/polylith/clj/core/util/edn_sanitizer_test.clj
+++ components/util/test/polylith/clj/core/util/edn_sanitizer_test.clj
@@ -3,9 +3,11 @@
             [polylith.clj.core.util.edn-sanitizer :as sut]))
 
 (deftest sanitize-keywords
-  (= {:abc 123
-      "@abc" 123})
-      "@nnn/@abc" 123
-     (sut/sanitize-keywords {:abc 123
-                             (keyword "@abc") 123
-                             (keyword "@nnn" "@abc") 123}))
+  (is (= {:abc 123
+          "@abc" 123
+          "@nnn/aaa" 123
+          "nnn/@bbb" 123}
+         (sut/sanitize-keywords {:abc 123
+                                 (keyword "@abc") 123
+                                 (keyword "@nnn" "aaa") 123
+                                 (keyword "nnn" "@bbb") 123}))))
