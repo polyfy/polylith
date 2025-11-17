3f27b3f8a8c2041a95d8b60fcf93944327c5c18d
Joakim Tengstrand <joakimtengstrand@gmail.com>
2025-11-17 10:49:25 +0100

    Try showing snapshot badge. 0.3.32-SNAPSHOT 2

--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -25,7 +25,7 @@
 (def minor 3)
 (def patch 32)
 (def revision SNAPSHOT) ;; Set to SNAPSHOT or RELEASE.
-(def snapshot 1) ;; Increase by one for every snapshot release, or set to 0 if a release.
+(def snapshot 2) ;; Increase by one for every snapshot release, or set to 0 if a release.
                   ;; Also update :snapshot-version: at the top of readme.adoc.
 (def snapshot? (= SNAPSHOT revision))
 
