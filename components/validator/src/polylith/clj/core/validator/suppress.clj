b91a75bb6539daad1ff3993fd69617befa5f46e1
Joakim Tengstrand <joakimtengstrand@gmail.com>
2026-09-06 05:46:55 +0200

    Update documentation

--- components/validator/src/polylith/clj/core/validator/suppress.clj
+++ components/validator/src/polylith/clj/core/validator/suppress.clj
@@ -1,6 +1,9 @@
 (ns polylith.clj.core.validator.suppress)
 
-(defn suppress? [{:keys [warning error bricks projects]} number {:keys [type name]}]
+(defn suppress?
+  "When neither :bricks nor :projects is given, the validation is suppressed for all entities.
+   When one of them is given, only the listed entities of that kind are suppressed."
+  [{:keys [warning error bricks projects]} number {:keys [type name]}]
   (and (or (= number warning)
            (= number error))
        (or (and (contains? #{"component" "base"} type)
