2e0f1712563a849bf98a862064b6b274314f5ca8
Joakim Tengstrand <joakimtengstrand@gmail.com>
2026-09-11 07:08:29 +0200

    Update transitive lib dependencies section in the doc.

--- components/version/src/polylith/clj/core/version/interface.clj
+++ components/version/src/polylith/clj/core/version/interface.clj
@@ -25,7 +25,7 @@
 (def minor 3)
 (def patch 33)
 (def revision SNAPSHOT) ;; Set to SNAPSHOT or RELEASE.
-(def snapshot 6) ;; Increase by one for every snapshot release, or set to 0 if a release.
+(def snapshot 7) ;; Increase by one for every snapshot release, or set to 0 if a release.
                   ;; Also update :snapshot-version: at the top of readme.adoc.
 (def snapshot? (= SNAPSHOT revision))
 
@@ -37,7 +37,7 @@
 
 (def tool (if system/extended? "polyx" "poly"))
 
-(def date "2026-09-10")
+(def date "2026-09-11")
 
 ;; Execute 'poly doc page:versions' to see when different changes was introduced.
 (def api-version {:breaking 1, :non-breaking 0})
